21356f80f7f612760767ec6d39e7f54c4b5854cd
Ubuntu <ubuntu@ip-172-31-5-9.ec2.internal>
2023-01-24 14:50:50 +0000

    pull containers to run example

---
+++ example/docker/pull.sh
@@ -0,0 +1,6 @@
+docker pull gnina/gnina
+docker pull ghcr.io/labdao/casf-2016-eval:main
+# avoided by adding some examples into git
+# aws s3 sync s3://labdao-benchmark/CASF-2016/coreset/1a30/ data
+
+
