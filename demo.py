ecd58eefb699a0f314f6f75e3b1acf42f24b55df
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2023-01-27 14:11:45 +0100

    adding scoring instruction

--- demo.py
+++ demo.py
@@ -2,9 +2,12 @@ import client
 import process
 
 if __name__ == "__main__":
-    print("generating diffdock instructions")
-    diffdock_instruct = client.generate_diffdock_instructions()
-    print("running diffdock")
-    process.main(diffdock_instruct)
-    #TODO: #29 add vina scoring
+    print("generating docking instructions")
+    docking_instruct = client.generate_diffdock_instructions()
+    print("running docking")
+    process.main(docking_instruct)
+    print("generating scoring instructions")
+    scoring_instruct = client.generate_vina_instructions()
+    print("running scoring")
+    process.main(scoring_instruct)
     #TODO: #30 add casf scoring
