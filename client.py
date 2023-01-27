a3e5a8fc6f4721557133a987f113a3377f775017
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2023-01-27 18:57:40 +0100

    include gnina instructions next to vina instructions

--- client.py
+++ client.py
@@ -54,5 +54,28 @@ def generate_vina_instructions(
     }
     return json.dumps(instructions)
 
+def generate_gnina_instructions(
+    debug_logs=True,
+    protein="1a30/1a30_protein.pdb",
+    ligand="1a30/1a30_ligand.sdf",
+    output="1a30/1a30_scored_gnina.sdf.gz",
+    cnn_scoring="rescore",
+    modifier="score_only",
+) -> dict:
+    instructions = {
+        "container_id": "gnina/gnina:latest",
+        "debug_logs": debug_logs,
+        "short_args": {"v": "/home/ubuntu/casf-2016:/inputs"},
+        "long_args": {"gpus": 0},
+        "cmd": (
+            "gnina -r"
+            f" /inputs/{protein} -l /inputs/{ligand} -o"
+            f" /inputs/{output}"
+            f" --autobox_ligand /inputs/{protein} --cnn_scoring {cnn_scoring} --exhaustiveness 64"
+            f" --{modifier}"
+        ),
+    }
+    return json.dumps(instructions)
+
 if __name__ == "__main__":
     print(generate_vina_instructions())
