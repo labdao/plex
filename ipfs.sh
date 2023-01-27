9ceb09c8c257baa1cc24b2015e13bbd4de335bc3
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2023-01-27 11:53:33 +0100

    skip last line

--- ipfs.sh
+++ ipfs.sh
@@ -13,5 +13,5 @@ ipfs cat /ipfs/QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc/readme
 # 127.0.0.1:5001/webui 
 # name files with ipfs get QmWLy5XUKuSVJiYirLfw3xSxD42BDPZEVvCv7f2mGanUiD -o test.png
 
-screen -dm ipfs daemon -D
 # NOTRUN screen -ls
+screen -dm ipfs daemon -D
