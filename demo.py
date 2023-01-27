e531fcd8a024bfa3ad8d708818ca014fe6357353
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2023-01-27 18:34:23 +0100

    generate scoring for complete directory

--- demo.py
+++ demo.py
@@ -1,13 +1,24 @@
 import client
 import process
+import os
 
 if __name__ == "__main__":
-    print("generating docking instructions")
-    docking_instruct = client.generate_diffdock_instructions()
-    print("running docking")
-    process.main(docking_instruct)
+    casf_list = os.listdir("/home/ubuntu/casf-2016")
+    # print("generating docking instructions")
+    # docking_instruct = client.generate_diffdock_instructions()
+    # print("running docking")
+    # process.main(docking_instruct)
+    instruction_list = []
     print("generating scoring instructions")
-    scoring_instruct = client.generate_vina_instructions()
+    for i in casf_list:
+        protein = i + '/' + i + '_protein.pdb'
+        ligand = i + '/' + i + '_ligand.sdf'
+        output = i + '/' + i + '_scored_vina.sdf.gz'
+        instruction_list.append(client.generate_vina_instructions(
+            protein=protein,
+            ligand=ligand,
+            output=output
+        ))
     print("running scoring")
-    process.main(scoring_instruct)
-    #TODO: #30 add casf scoring
+    for instruction in instruction_list:
+        process.main(instruction)
