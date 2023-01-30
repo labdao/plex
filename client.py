26d9071a72a005996d0b9a6e226ec1165b0fd322
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2023-01-30 19:07:16 +0100

    run diffdock by ticket - add demo with filter, change client params

--- client.py
+++ client.py
@@ -3,11 +3,9 @@ import json
 
 def generate_diffdock_instructions(
     debug_logs=True,
-    protein_path="inputs/test.pdb",
-    fasta_out_file="outputs/prepared_for_esm.fasta",
-    ligand="inputs/test.sdf",
+    protein="1a30/1a30_protein.pdb",
+    ligand="1a30/1a30_ligand.sdf",
     repr_layers=33,
-    out_dir="outputs",
     inference_steps=20,
     samples_per_complex=40,
     batch_size=10,
@@ -20,12 +18,12 @@ def generate_diffdock_instructions(
         "long_args": {"gpus": "all"},
         "cmd": (
             '/bin/bash -c \"'
-            f'python datasets/esm_embedding_preparation.py --protein_path {protein_path} --out_file {fasta_out_file} '
-            f'&& HOME=esm/model_weights python esm/scripts/extract.py esm2_t33_650M_UR50D {fasta_out_file} '
-            f'outputs/esm2_output --repr_layers {repr_layers} --include per_tok && '
-            f'python -m inference --protein_path {protein_path} --ligand {ligand} --out_dir {out_dir} '
+            f'python datasets/esm_embedding_preparation.py --protein_path inputs/{protein_path} --out_file esm2_input.fasta'
+            f'&& HOME=esm/model_weights python esm/scripts/extract.py esm2_t33_650M_UR50D inputs/esm2_input.fasta'
+            f'inputs/esm2_output --repr_layers {repr_layers} --include per_tok && '
+            f'python -m inference --protein_path inputs/{protein_path} --ligand inputs/{ligand} --out_dir /inputs '
             f'--inference_steps {inference_steps} --samples_per_complex {samples_per_complex} --batch_size {batch_size} '
-            f'--actual_steps {actual_steps} --esm_embeddings_path outputs/esm2_output --no_final_step_noise\"'
+            f'--actual_steps {actual_steps} --esm_embeddings_path inputs/esm2_output --no_final_step_noise\"'
         ),
     }
     return json.dumps(instructions)
--- demo.py
+++ demo.py
@@ -3,15 +3,30 @@ import process
 import os
 import json
 
+BASEDIR = "/home/ubuntu/PDBBind_processed"
+
 if __name__ == "__main__":
-    casf_list = os.listdir("/home/ubuntu/casf-2016")
-    # print("generating docking instructions")
-    # docking_instruct = client.generate_diffdock_instructions()
+    task_list = os.listdir(BASEDIR)
+    testset_df = pd.read_csv(BASEDIR + '/testset_csv.csv')
+    filtered_task_list = [i.split('/')[-2] for i in df['protein_path'].tolist() if i.split('/')[-2] in task_list]
+    print("generating docking instructions for diffdock")
+    docking_instruct = client.generate_diffdock_instructions()
+    for i in filtered_task_list:
+        protein = i + '/' + i + '_protein.pdb'
+        ligand = i + '/' + i + '_ligand.sdf'
+        output = i + '/' + i + '_scored_vina.sdf.gz'
+        instruction = client.generate_vina_instructions(
+            protein=protein,
+            ligand=ligand,
+            output=output
+        )
+        print(instruction)
+        instruction_list.append(instruction)
     # print("running docking")
     # process.main(docking_instruct)
     instruction_list = []
     print("generating scoring instructions for vina")
-    for i in casf_list:
+    for i in filtered_task_list:
         protein = i + '/' + i + '_protein.pdb'
         ligand = i + '/' + i + '_ligand.sdf'
         output = i + '/' + i + '_scored_vina.sdf.gz'
@@ -23,7 +38,7 @@ if __name__ == "__main__":
         print(instruction)
         instruction_list.append(instruction)
     print("generating scoring instructions for gnina")
-    for i in casf_list:
+    for i in filtered_task_list:
         protein = i + '/' + i + '_protein.pdb'
         ligand = i + '/' + i + '_ligand.sdf'
         output = i + '/' + i + '_scored_gnina.sdf.gz'
