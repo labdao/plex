fa640739bdca6b5ef508aa3a14e1d00d68b8cfca
thetechnocrat-dev <josh.mcmenemy@openzyme.bio>
2023-02-14 19:12:16 +0000

    create bacalhau job using bacalhau go pkg

--- plex/bacalhau.go
+++ plex/bacalhau.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os/exec"
 	"strings"
+
+	"github.com/filecoin-project/bacalhau/pkg/model"
 )
 
 func InstructionToBacalhauCmd(cid, container, cmd string) string {
@@ -11,14 +13,29 @@ func InstructionToBacalhauCmd(cid, container, cmd string) string {
 	return `bacalhau docker run --network full --gpu 1 --memory 12gb -i ` + fmt.Sprintf(cid) + ` ` + fmt.Sprintf(container) + ` -- ` + fmt.Sprintf(cmd)
 }
 
-func RunBacalhauCmd(cmdString string) {
+func createBacalhauJob(cid, container, cmd string) (job *model.Job, err error) {
+	job, err = model.NewJobWithSaneProductionDefaults()
+	if err != nil {
+		return nil, err
+	}
+	job.Spec.Docker.Image = container
+	job.Spec.Docker.Entrypoint = []string{cmd}
+	job.Spec.Network = model.NetworkConfig{Type: model.NetworkFull}
+	job.Spec.Resources.Memory = "12gb"
+	job.Spec.Resources.GPU = "1"
+	job.Spec.Inputs = []model.StorageSpec{{CID: cid}}
+	return job, err
+}
+
+func RunBacalhauCmd(cmdString string) (out []byte, err error) {
 	args := strings.Fields(cmdString)
 	fmt.Println(args)
 	cmd := exec.Command(args[0], args[1:]...)
-	out, err := cmd.CombinedOutput()
+	out, err = cmd.CombinedOutput()
 	if err != nil {
 		fmt.Printf("Command failed: %s\n", err)
 		return
 	}
 	fmt.Printf("Output: %s\n", out)
+	return
 }
--- plex/bacalhau_test.go
+++ plex/bacalhau_test.go
@@ -12,3 +12,31 @@ func TestInstructionToBacalhauCmd(t *testing.T) {
 		t.Errorf("got = %s; wanted %s", fmt.Sprint(got), fmt.Sprint(want))
 	}
 }
+
+func TestCreatevalhauJob(t *testing.T) {
+	cid := "bafybeig7rsafgrtwzivrorumixcqxpwmje7cp56eoxzg3jbwxxyy26xgue"
+	container := "ubuntu"
+	cmd := "echo DeSci"
+	job, err := createBacalhauJob(cid, container, cmd)
+	if err != nil {
+		t.Fatalf(fmt.Sprint(err))
+	}
+	if job.Spec.Docker.Image != container {
+		t.Errorf("got = %s; wanted %s", fmt.Sprint(job.Spec.Docker.Image), fmt.Sprint(container))
+	}
+	if job.Spec.Docker.Entrypoint[0] != cmd {
+		t.Errorf("got = %s; wanted %s", fmt.Sprint(job.Spec.Docker.Entrypoint), fmt.Sprint(cmd))
+	}
+	if job.Spec.Inputs[0].CID != cid {
+		t.Errorf("got = %s; wanted %s", fmt.Sprint(job.Spec.Inputs[0].CID), fmt.Sprint(cid))
+	}
+}
+
+func TestRunBacalhauCmd(t *testing.T) {
+	cmd := "bacalhau docker run ubuntu echo Hello World"
+	out, err := RunBacalhauCmd(cmd)
+	if err != nil {
+		t.Fatalf(fmt.Sprint(err))
+	}
+	fmt.Printf("Output: %s\n", out)
+}
