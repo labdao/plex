117989b5ca92eaba5407a79542fdfc851b1ce31c
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-02-16 15:50:25 -0500

    install script closing messages added and plex README

--- plex/install.sh
+++ plex/install.sh
@@ -121,4 +121,7 @@ installBacalhau
 setW3SToken
 buildPlexBinary
 displayLogo
+
 echo "Installation complete. Welcome to LabDAO! Documentation at https://github.com/labdao/ganglia"
+echo "To start using Plex, run the following command:"
+echo "./plex -app equibind -input-dir ./testdata/pdbbind_processed_size1"
