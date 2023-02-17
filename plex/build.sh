341a09ad05bcbb41876105e87216f85bdf2bdc96
thetechnocrat-dev <josh.mcmenemy@openzyme.bio>
2023-02-16 21:55:09 -0500

    new builds

--- plex/build.sh
+++ plex/build.sh
@@ -1,27 +1,27 @@
-buildMacOsBinaries() {
-    echo "Building Mac OS binaries..."
-    
-    if test releases/macos-amd64/plex ; then
-        rm releases/macos-amd64/plex
-    fi
-
-    if test releases/macos-arm64/plex ; then
-        rm releases/macos-arm64/plex
-    fi
-
-    GOOS=darwin GOARCH=amd64 go build -o releases/macos-amd64
-    GOOS=darwin GOARCH=arm64 go build -o releases/macos-arm64
-}
-
-buildLinuxBinaries() {
-    echo "Building Linux binaries..."
-
-    if test releases/linux-amd64/plex ; then
-        rm releases/linux-amd64/plex
-    fi
-
-    GOOS=linux GOARCH=amd64 go build -o releases/linux-amd64
-}
+# buildMacOsBinaries() {
+#     echo "Building Mac OS binaries..."
+#     
+#     if test releases/macos-amd64/plex ; then
+#         rm releases/macos-amd64/plex
+#     fi
+# 
+#     if test releases/macos-arm64/plex ; then
+#         rm releases/macos-arm64/plex
+#     fi
+# 
+#     GOOS=darwin GOARCH=amd64 go build -o releases/macos-amd64
+#     GOOS=darwin GOARCH=arm64 go build -o releases/macos-arm64
+# }
+# 
+# buildLinuxBinaries() {
+#     echo "Building Linux binaries..."
+# 
+#     if test releases/linux-amd64/plex ; then
+#         rm releases/linux-amd64/plex
+#     fi
+# 
+#     GOOS=linux GOARCH=amd64 go build -o releases/linux-amd64
+# }
 
 buildWindowsBinaries() {
     echo "Building Windows binaries..."
@@ -30,11 +30,11 @@ buildWindowsBinaries() {
         rm releases/windows-amd64/plex.exe
     fi
 
-    GOOS=windows GOARCH=amd64 go build -o releases/windows-amd64
+    GOOS=windows GOARCH=amd64 go build -o releases/windows-amd64/
 }
 
-buildMacOsBinaries
-buildLinuxBinaries
+# buildMacOsBinaries
+# buildLinuxBinaries
 buildWindowsBinaries
 
 echo "All builds complete."
