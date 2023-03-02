ce880330f62edaaaf759a820d8256e0a830e4efe
The Technocrat <josh.mcmenemy@openzyme.bio>
2023-03-02 04:47:20 -0500

    add directory to ipfs node via http (#120)

--- internal/ipfs/ipfs_test.go
+++ internal/ipfs/ipfs_test.go
@@ -34,3 +34,17 @@ func TestPutFile(t *testing.T) {
 		)
 	}
 }
+
+func TestAddDirHttp(t *testing.T) {
+	expectedCid := "QmWVKoVYBWHWdRLrL8Td5kUpqN2qH6zQ5piwtdCE1fjSYt"
+	actualCid, err := AddDirHttp(os.Getenv("IPFS_NODE_URL"), "../../testdata/ipfs_test")
+	if err != nil {
+		t.Fatalf("error creating client: %v", err)
+	}
+	if expectedCid != actualCid {
+		t.Errorf(`unmatching cids
+			expected CID: %s
+			actual CID: %s`, expectedCid, actualCid,
+		)
+	}
+}
