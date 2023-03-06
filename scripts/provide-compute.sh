46a2803ab350552569baeddad6ed95e0835f58be
thetechnocrat-dev <josh.mcmenemy@openzyme.bio>
2023-03-06 17:24:55 -0500

    move provide compute logic to functions

--- scripts/provide-compute.sh
+++ scripts/provide-compute.sh
@@ -7,113 +7,132 @@
 set -E
 trap '[ "$?" -ne 77 ] || exit 77' ERR
 
-# https://unix.stackexchange.com/questions/48533/exit-shell-script-from-a-subshell
-set -E
-trap '[ "$?" -ne 77 ] || exit 77' ERR
-
 # Docker install directions from https://docs.docker.com/engine/install/ubuntu/
-echo "Installing Docker"
-sudo apt-get update
-sudo apt-get install -y \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y  docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo groupadd -f docker
-sudo usermod -aG docker $USER
+installDocker() {
+    echo "Installing Docker"
+    sudo apt-get update
+    sudo apt-get install -y \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y  docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    sudo groupadd -f docker
+    sudo usermod -aG docker $USER
+}
 
-/usr/bin/newgrp docker <<EONG
-echo "Testing Docker Install"
-if docker run hello-world ; then
-    echo "Docker succesfully installed"
-else
-    echo "Docker install failed"
-    exit 77
-fi
+testDockerInstall() {
+    echo "Testing Docker Install"
+    /usr/bin/newgrp docker <<EONG
+    if docker run hello-world ; then
+        echo "Docker succesfully installed"
+    else
+        echo "Docker install failed"
+        exit 77
+    fi
 EONG
+}
 
-# Nvidia driver instructions from https://docs.nvidia.com/datacenter/tesla/tesla-installation-notes/index.html
-echo "Installing Nvidia Drivers"
-sudo apt install -y build-essential
-sudo apt-get install linux-headers-$(uname -r)
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
-wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.0-1_all.deb
-sudo dpkg -i cuda-keyring_1.0-1_all.deb
-sudo apt-get update
-sudo apt-get -y install cuda-drivers
-export PATH=/usr/local/cuda-12.0/bin${PATH:+:${PATH}}
-sudo /usr/bin/nvidia-persistenced --verbose
+installNvidiaDrivers() {
+    # Nvidia driver instructions from https://docs.nvidia.com/datacenter/tesla/tesla-installation-notes/index.html
+    echo "Installing Nvidia Drivers"
+    sudo apt install -y build-essential
+    sudo apt-get install linux-headers-$(uname -r)
+    distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+    wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.0-1_all.deb
+    sudo dpkg -i cuda-keyring_1.0-1_all.deb
+    sudo apt-get update
+    sudo apt-get -y install cuda-drivers
+    export PATH=/usr/local/cuda-12.0/bin${PATH:+:${PATH}}
+    sudo /usr/bin/nvidia-persistenced --verbose
+}
 
-echo "Testing Nvidia Driver Install"
-if  cat /proc/driver/nvidia/version; then
-    echo "Nvidia succesfully installed"
-else
-    echo "Nvidia install failed"
-    exit
-fi
+testInstallNvidia() {
+    echo "Testing Nvidia Driver Install"
+    if cat /proc/driver/nvidia/version; then
+        echo "Nvidia succesfully installed"
+    else
+        echo "Nvidia install failed"
+        exit
+    fi
+}
 
-# Nvidia Container Toolkit instructions from https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html
-echo "Installing Nvidia Container Toolkit"
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --yes --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
-            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
-sudo apt-get update
-sudo apt-get install -y nvidia-docker2
-sudo systemctl restart docker
+installNvidiaContainerToolKit() {
+    # Nvidia Container Toolkit instructions from https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html
+    echo "Installing Nvidia Container Toolkit"
+    distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+          && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --yes --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+          && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+                sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+                sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+    sudo apt-get update
+    sudo apt-get install -y nvidia-docker2
+    sudo systemctl restart docker
+}
 
-/usr/bin/newgrp docker <<EONG
-echo "Testing Nvidia Container Toolkit Install"
-if docker run --gpus all nvidia/cuda:11.6.2-base-ubuntu20.04 nvidia-smi ; then
-    echo "Nvidia Container Toolkit succesfully installed"
-else
-    echo "Nvidia Container Toolkit install failed"
-    exit 77
-fi
+testNvidiaContainerToolkitInstall() {
+    /usr/bin/newgrp docker <<EONG
+    echo "Testing Nvidia Container Toolkit Install"
+    if docker run --gpus all nvidia/cuda:11.6.2-base-ubuntu20.04 nvidia-smi ; then
+        echo "Nvidia Container Toolkit succesfully installed"
+    else
+        echo "Nvidia Container Toolkit install failed"
+        exit 77
+    fi
 EONG
+}
 
-# Install GoLang
-echo "Installing GoLang"
-wget https://go.dev/dl/go1.19.6.linux-amd64.tar.gz
-sudo tar -C /usr/local -xvzf go1.19.6.linux-amd64.tar.gz
-rm go1.19.6.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+installGolang() {
+    echo "Installing GoLang"
+    wget https://go.dev/dl/go1.19.6.linux-amd64.tar.gz
+    sudo tar -C /usr/local -xvzf go1.19.6.linux-amd64.tar.gz
+    rm go1.19.6.linux-amd64.tar.gz
+    export PATH=$PATH:/usr/local/go/bin
+}
 
-echo "Testing GoLang Install"
-if go version ; then
-    echo "GoLang succesfully installed "
-else
-    echo "GoLang install failed"
-    exit 77
-fi
+testGolangInstall() {
+    echo "Testing GoLang Install"
+    if go version ; then
+        echo "GoLang succesfully installed "
+    else
+        echo "GoLang install failed"
+        exit 77
+    fi
+}
 
-# IPFS setup
-echo "Installing IPFS"
-wget https://dist.ipfs.tech/kubo/v0.18.0/kubo_v0.18.0_linux-amd64.tar.gz
-tar -xvzf kubo_v0.18.0_linux-amd64.tar.gz
-cd kubo
-sudo bash install.sh
-ipfs --version
+installIPFS() {
+    echo "Installing IPFS"
+    wget https://dist.ipfs.tech/kubo/v0.18.0/kubo_v0.18.0_linux-amd64.tar.gz
+    tar -xvzf kubo_v0.18.0_linux-amd64.tar.gz
+    cd kubo
+    sudo bash install.sh
+    ipfs --version
+    ipfs cat /ipfs/QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc/readme
+}
 
-# post installation
-ipfs init
-ipfs cat /ipfs/QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc/readme
+testIPFSInstall() {
+    echo "Testing IPFS Install"
+    if ipfs --version ; then
+        echo "IPFS succesfully installed "
+    else
+        echo "IPFS install failed"
+        exit 77
+    fi
+}
 
-echo "Starting IPFS Daemon"
-screen -dm ipfs daemon
+runIPFS() {
+    echo "Starting IPFS Daemon"
+    ipfs init
+    screen -dm ipfs daemon
+}
 
-# pip installation
-echo "Installing pip"
-sudo apt install python3-pip
-
+printLogo() {
 logo="
                                         @
                                  @@@@@@@@@@@@@@@
@@ -147,7 +166,22 @@ logo="
                               @@@@@@@@@@@@@@@@@@@@@
                                @@@@@@@@@@@@@@@@@@@
                                  @@@@@@@@@@@@@@@
-                                        @
-"
-echo "$logo"
-echo "Welcome to LabDAO! Documentation at https://github.com/labdao/"
+                                        @"
+  echo "$logo"
+  echo "Welcome to LabDAO! Documentation at https://github.com/labdao/"
+}
+
+def setup() {
+    installDocker
+    testDockerInstall
+    installNvidiaDrivers
+    testInstallNvidia
+    installNvidiaContainerToolKit
+    testNvidiaContainerToolkitInstall
+    testIPFSInstall
+    printLogo
+}
+
+def start() {
+    runIPFS
+}
