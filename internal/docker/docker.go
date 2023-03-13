86574663946b922f6ee029fdef29d74915caf8d7
thetechnocrat-dev <josh.mcmenemy@openzyme.bio>
2023-03-12 21:24:34 -0400

    remove unused function

--- internal/docker/docker.go
+++ internal/docker/docker.go
@@ -1,9 +1,7 @@
 package docker
 
 import (
-	"bufio"
 	"fmt"
-	"os/exec"
 	"path/filepath"
 )
 
@@ -14,31 +12,4 @@ func InstructionToDockerCmd(container, cmd, jobDir string, gpu bool) string {
 		gpuFlag = "--gpus"
 	}
 	return fmt.Sprintf("docker run %s -v %s:/inputs -v %s:/outputs %s /bin/bash -c '%s'", gpuFlag, jobDir, outputsDir, container, cmd)
-}
-
-func RunDockerCmd(container, cmd, jobDir string, gpu bool) error {
-	dockerCmd := InstructionToDockerCmd(container, cmd, jobDir, gpu)
-
-	// make output dir
-
-	cmdExec := exec.Command(dockerCmd)
-
-	stdout, err := cmdExec.StdoutPipe()
-	if err != nil {
-		return err
-	}
-
-	if err := cmdExec.Start(); err != nil {
-		return err
-	}
-
-	scanner := bufio.NewScanner(stdout)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-
-	if err := cmdExec.Wait(); err != nil {
-		return err
-	}
-	return err
 }
