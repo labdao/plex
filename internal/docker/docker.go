9dae6237a83b69c683179e6f8fa38a903fb7fd28
thetechnocrat-dev <josh.mcmenemy@openzyme.bio>
2023-03-14 16:46:49 -0400

    docker run works

--- internal/docker/docker.go
+++ internal/docker/docker.go
@@ -1,15 +1,48 @@
 package docker
 
 import (
+	"bufio"
 	"fmt"
+	"os/exec"
 	"path/filepath"
 )
 
 func InstructionToDockerCmd(container, cmd, jobDir string, gpu bool) string {
 	outputsDir := filepath.Join(jobDir, "outputs")
+	fmt.Println(outputsDir)
 	gpuFlag := ""
 	if gpu {
 		gpuFlag = "--gpus"
 	}
 	return fmt.Sprintf("docker run %s -v %s:/inputs -v %s:/outputs %s /bin/bash -c '%s'", gpuFlag, jobDir, outputsDir, container, cmd)
+}
+
+func RunDockerJob(container, cmd, jobDir string, gpu bool) error {
+	fmt.Println(InstructionToDockerCmd(container, cmd, jobDir, gpu))
+	dockerCmd := InstructionToDockerCmd(container, cmd, jobDir, gpu)
+	cmdExec := exec.Command("/bin/sh", "-c", dockerCmd)
+
+	// Set up a pipe to read the command's stdout
+	stdout, err := cmdExec.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	// Start the command
+	if err := cmdExec.Start(); err != nil {
+		fmt.Println("Error starting Docker command:", err)
+		return err
+	}
+
+	// Use a scanner to read the output line by line
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	// Wait for the command to finish
+	if err := cmdExec.Wait(); err != nil {
+		return err
+	}
+	return nil
 }
