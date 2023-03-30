3fb95b2704d5f8d396c27847418f301a1c70ff10
thetechnocrat-dev <josh.mcmenemy@openzyme.bio>
2023-03-30 00:18:10 -0400

    tool to docker cmd works

--- internal/ipwl/tool.go
+++ internal/ipwl/tool.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
+	"strings"
 )
 
 type Tool struct {
@@ -37,4 +39,27 @@ func readToolConfig(filePath string) (Tool, error) {
 	}
 
 	return tool, nil
+}
+
+func ToolToDockerCmd(toolConfig Tool, ioEntry IO, outputDirPath string) (string, error) {
+	inputVolumes := ""
+	for _, input := range ioEntry.Inputs {
+		inputFilepath := input.(map[string]interface{})["filepath"].(string)
+		inputVolumes += fmt.Sprintf("-v %s:/inputs ", inputFilepath)
+	}
+
+	arguments := strings.Join(toolConfig.Arguments, " ")
+
+	placeholderRegex := regexp.MustCompile(`\$\((inputs\..+?\.filepath)\)`)
+	matches := placeholderRegex.FindAllStringSubmatch(arguments, -1)
+
+	for _, match := range matches {
+		placeholder := match[0]
+		key := strings.TrimSuffix(strings.TrimPrefix(match[1], "inputs."), ".filepath")
+		arguments = strings.Replace(arguments, placeholder, ioEntry.Inputs[key].(map[string]interface{})["filepath"].(string), -1)
+	}
+
+	dockerCmd := fmt.Sprintf("docker %s-v %s:/outputs run %s %s %s", inputVolumes, outputDirPath, toolConfig.DockerPull, strings.Join(toolConfig.BaseCommand, " "), arguments)
+
+	return dockerCmd, nil
 }
--- internal/ipwl/tool_test.go
+++ internal/ipwl/tool_test.go
@@ -12,10 +12,11 @@ func TestReadToolConfig(t *testing.T) {
 		Description: "test tool",
 		BaseCommand: []string{"/bin/bash", "-c"},
 		Arguments: []string{
-			"python main.py --protein $(inputs.protein.path) --small_molecule_library $(inputs.small_molecule.path);",
-			"cp /inputs /outputs;",
+			"python main.py --protein $(inputs.protein.filepath) --small_molecule_library $(inputs.small_molecule.filepath);",
+			"mv /outputs/ligands_predicted.sdf /outputs/$(inputs.protein.filepath)_$(inputs.small_molecule.filepath)_docked.$(inputs.small_molecule.filepath);",
+			"cp /inputs/$(inputs.protein.filepath) /outputs/",
 		},
-		DockerPull: "ghcr.io/labdao/testtool",
+		DockerPull: "ghcr.io/labdao/equibind@sha256:ae2cec63b3924774727ed1c6c8af95cf4aaea2d3f0c5acbec56478505ccb2b07",
 		GpuBool:    false,
 		Inputs: map[string]interface{}{
 			"protein": map[string]interface{}{
@@ -46,5 +47,37 @@ func TestReadToolConfig(t *testing.T) {
 
 	if !reflect.DeepEqual(tool, expected) {
 		t.Errorf("Expected:\n%v\nGot:\n%v", expected, tool)
+	}
+}
+
+func TestToolToDockerCmd(t *testing.T) {
+	toolConfig := Tool{
+		DockerPull:  "some_docker_pull",
+		BaseCommand: []string{"some_base_command"},
+		Arguments:   []string{"--protein", "$(inputs.protein.filepath)", "--small_molecule", "$(inputs.small_molecule.filepath)"},
+	}
+
+	ioEntry := IO{
+		Inputs: map[string]interface{}{
+			"protein": map[string]interface{}{
+				"filepath": "testdata/binding/abl/7n9g.pdb",
+			},
+			"small_molecule": map[string]interface{}{
+				"filepath": "testdata/binding/abl/ZINC000003986735.sdf",
+			},
+		},
+	}
+
+	outputDirPath := "some_output_dir"
+
+	dockerCmd, err := ToolToDockerCmd(toolConfig, ioEntry, outputDirPath)
+	if err != nil {
+		t.Errorf("Error generating Docker command: %v", err)
+	}
+
+	expectedDockerCmd := "docker -v testdata/binding/abl/7n9g.pdb:/inputs -v testdata/binding/abl/ZINC000003986735.sdf:/inputs -v some_output_dir:/outputs run some_docker_pull some_base_command --protein testdata/binding/abl/7n9g.pdb --small_molecule testdata/binding/abl/ZINC000003986735.sdf"
+
+	if dockerCmd != expectedDockerCmd {
+		t.Errorf("Expected Docker command: %s, got: %s", expectedDockerCmd, dockerCmd)
 	}
 }
