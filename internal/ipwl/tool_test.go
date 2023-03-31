ff9ca4cd1009f430ae528068e0006c80cb2342f8
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-03-31 10:37:35 -0400

    tool_test passing

--- internal/ipwl/tool_test.go
+++ internal/ipwl/tool_test.go
@@ -8,34 +8,35 @@ import (
 func TestReadToolConfig(t *testing.T) {
 	filePath := "testdata/example_tool.json"
 	expected := Tool{
-		Name:        "test",
-		Description: "test tool",
+		Name:        "equibind",
+		Description: "test for equibind tool",
 		BaseCommand: []string{"/bin/bash", "-c"},
 		Arguments: []string{
 			"python main.py --protein $(inputs.protein.filepath) --small_molecule_library $(inputs.small_molecule.filepath);",
-			"mv /outputs/ligands_predicted.sdf /outputs/$(inputs.protein.filepath)_$(inputs.small_molecule.filepath)_docked.$(inputs.small_molecule.filepath);",
-			"cp /inputs/$(inputs.protein.filepath) /outputs/",
+			"mv /outputs/ligands_predicted.sdf /outputs/$(inputs.protein.basename)_$(inputs.small_molecule.basename)_docked.$(inputs.small_molecule.ext);",
+			"cp $(inputs.protein.filepath) /outputs/;",
+			"rmdir /outputs/dummy;",
 		},
 		DockerPull: "ghcr.io/labdao/equibind@sha256:ae2cec63b3924774727ed1c6c8af95cf4aaea2d3f0c5acbec56478505ccb2b07",
 		GpuBool:    false,
-		Inputs: map[string]interface{}{
-			"protein": map[string]interface{}{
-				"type": "File",
-				"glob": "*.pdb",
+		Inputs: map[string]ToolInput{
+			"protein": {
+				Type: "File",
+				Glob: []string{"*.pdb"},
 			},
-			"small_molecule": map[string]interface{}{
-				"type": "File",
-				"glob": []interface{}{"*.sdf", "*.mol"},
+			"small_molecule": {
+				Type: "File",
+				Glob: []string{"*.sdf", "*.mol2"},
 			},
-			"outputs": map[string]interface{}{
-				"docked_small_molecule": map[string]interface{}{
-					"type": "File",
-					"glob": "*_docked.sdf",
-					"protein": map[string]interface{}{
-						"type": "File",
-						"glob": "*.pdb",
-					},
-				},
+		},
+		Outputs: map[string]ToolOutput{
+			"docked_small_molecule": {
+				Type: "File",
+				Glob: []string{"*_docked.sdf"},
+			},
+			"protein": {
+				Type: "File",
+				Glob: []string{"*.pdb"},
 			},
 		},
 	}
@@ -58,24 +59,25 @@ func TestToolToDockerCmd(t *testing.T) {
 	}
 
 	ioEntry := IO{
-		Inputs: map[string]interface{}{
-			"protein": map[string]interface{}{
-				"filepath": "testdata/binding/abl/7n9g.pdb",
+		Inputs: map[string]FileInput{
+			"protein": {
+				FilePath: "testdata/binding/abl/7n9g.pdb",
 			},
-			"small_molecule": map[string]interface{}{
-				"filepath": "testdata/binding/abl/ZINC000003986735.sdf",
+			"small_molecule": {
+				FilePath: "testdata/binding/abl/ZINC000003986735.sdf",
 			},
 		},
 	}
 
-	outputDirPath := "some_output_dir"
+	inputsDirPath := "testdata/binding/abl"
+	outputsDirPath := "some_output_dir"
 
-	dockerCmd, err := toolToDockerCmd(toolConfig, ioEntry, outputDirPath)
+	dockerCmd, err := toolToDockerCmd(toolConfig, ioEntry, inputsDirPath, outputsDirPath)
 	if err != nil {
 		t.Errorf("Error generating Docker command: %v", err)
 	}
 
-	expectedDockerCmd := "docker -v testdata/binding/abl/7n9g.pdb:/inputs -v testdata/binding/abl/ZINC000003986735.sdf:/inputs -v some_output_dir:/outputs run some_docker_pull some_base_command --protein testdata/binding/abl/7n9g.pdb --small_molecule testdata/binding/abl/ZINC000003986735.sdf"
+	expectedDockerCmd := "docker run -v testdata/binding/abl:/inputs -v some_output_dir:/outputs some_docker_pull some_base_command \"--protein /inputs/7n9g.pdb --small_molecule /inputs/ZINC000003986735.sdf\""
 
 	if dockerCmd != expectedDockerCmd {
 		t.Errorf("Expected Docker command: %s, got: %s", expectedDockerCmd, dockerCmd)
