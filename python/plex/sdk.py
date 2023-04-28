03ece727ed1ccc1252bacaea1393784655ee87bf
thetechnocrat-dev <josh.mcmenemy@openzyme.bio>
2023-04-27 22:21:47 -0400

    add print status to python sdk

--- python/plex/sdk.py
+++ python/plex/sdk.py
@@ -27,3 +27,22 @@ def run_plex(io: Union[Dict, List[Dict]]):
         with subprocess.Popen(cmd, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True, cwd=plex_dir) as p:
             for line in p.stdout:
                 print(line, end='')
+
+
+def print_io_graph_status(io_graph):
+    state_count = {}
+
+    # Iterate through the io_list and count the occurrences of each state
+    for io in io_graph:
+        state = io['state']
+        if state in state_count:
+            state_count[state] += 1
+        else:
+            state_count[state] = 1
+
+    # Print the total number of IOs
+    print(f"Total IOs: {len(io_graph)}")
+
+    # Print the number of IOs in each state
+    for state, count in state_count.items():
+        print(f"IOs in {state} state: {count}")
