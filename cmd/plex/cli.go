f28014817bf2e10bc915dda8ef046b26bac4cb95
thetechnocrat-dev <josh.mcmenemy@openzyme.bio>
2023-05-02 13:21:58 -0700

    better logs

--- cmd/plex/cli.go
+++ cmd/plex/cli.go
@@ -79,8 +79,8 @@ func Run(toolPath, inputDir, ioJsonPath, workDir string, verbose, retry, local b
 	fmt.Println("Initialized IO file at: ", ioJsonPath)
 
 	fmt.Println("Processing IO Entries")
-	fmt.Println("workdDirPath")
-	fmt.Println("ioJsonPath")
+	fmt.Println(workDirPath)
+	fmt.Println(ioJsonPath)
 	ipwl.ProcessIOList(workDirPath, ioJsonPath, retry, verbose, local, concurrency)
 	fmt.Printf("Finished processing, results written to %s", ioJsonPath)
 }
