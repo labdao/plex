b8e49c18837cd55423e679b72c80bf5b23ecc80a
thetechnocrat-dev <josh.mcmenemy@openzyme.bio>
2023-05-06 15:15:26 -0400

    update plex sdk

--- python/plex/sdk.py
+++ python/plex/sdk.py
@@ -6,7 +6,7 @@ from tempfile import TemporaryDirectory
 from typing import Dict, List, Union
 
 
-def run_plex(io: Union[Dict, List[Dict]], concurrency=1, local=False):
+def run_plex(io: Union[Dict, List[Dict]], concurrency=1, local=False, verbose=False, retry=False):
     if not (isinstance(io, dict) or (isinstance(io, list) and all(isinstance(i, dict) for i in io))):
         raise ValueError('io must be a dict or a list of dicts')
 
@@ -23,8 +23,16 @@ def run_plex(io: Union[Dict, List[Dict]], concurrency=1, local=False):
         cwd = os.getcwd()
         plex_dir = os.path.dirname(os.path.dirname(cwd))
         cmd = ["./plex", "-input-io", json_file_path, "-concurrency", str(concurrency)]
+
         if local:
             cmd.append("-local=true")
+
+        if verbose:
+            cmd.append("-verbose=true")
+
+        if retry:
+            cmd.append("-retry=true")
+
         with subprocess.Popen(cmd, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True, cwd=plex_dir) as p:
             for line in p.stdout:
                 print(line, end='')
