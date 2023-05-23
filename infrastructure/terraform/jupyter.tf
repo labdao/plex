cdad3ed7b797dc5e3f701ab31c56d055a6377e75
Humberto Evans <hevans66@gmail.com>
2023-05-23 09:51:36 -0700

    bump jupyter instance to 1TB disk and t3.xlarge

--- infrastructure/terraform/jupyter.tf
+++ infrastructure/terraform/jupyter.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "plex_jupyter" {
   for_each      = toset(["littlehub"])
   ami           = var.ami_main
-  instance_type = "t3.micro"
+  instance_type = "t3.xlarge"
 
   vpc_security_group_ids = [aws_security_group.plex.id]
   key_name               = var.key_main
   availability_zone      = var.availability_zones[0]
 
   root_block_device {
-    volume_size = 10
+    volume_size = 1000
   }
 
   tags = {
