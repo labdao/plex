864185d1054944339068447d43f7ef86e801c9b2
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-05-24 15:51:31 -0400

    hardhat config set for optimism goerli network for deployment prep

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    optimismGoerli: {
+      url: `https://opt-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [WALLET_PRIVATE_KEY]
+    }
+  }
 };
