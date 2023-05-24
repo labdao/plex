8f740c14bd2abe97c2f84da0f7ce054eaeceadc0
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-05-24 15:13:05 -0400

    contract deploy script modified

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -7,26 +7,17 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const ProofOfScience = await hre.ethers.getContractFactory("ProofOfScience");
+  const proofOfScience = await ProofOfScience.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  await proofOfScience.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("ProofOfScience deployed to:", proofOfScience.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
