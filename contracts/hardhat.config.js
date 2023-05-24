baa56ffab38998d5f32e78fe510f6dcbfcfbfaa2
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-05-24 16:35:44 -0400

    config modified to take in block explorer api key

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
+const OPTIMISM_BLOCK_EXPLORER_API_KEY = process.env.OPTIMISM_BLOCK_EXPLORER_API_KEY;
 
 module.exports = {
   solidity: "0.8.18",
@@ -13,5 +14,8 @@ module.exports = {
       url: `https://opt-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [WALLET_PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: OPTIMISM_BLOCK_EXPLORER_API_KEY
   }
 };
