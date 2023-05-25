ef49585bde22dbeede196236d8aa8f2376297ab7
The Technocrat <josh.mcmenemy@openzyme.bio>
2023-05-25 16:46:24 -0400

    add generate io json helper function (#335)

--- python/plex/sdk.py
+++ python/plex/sdk.py
@@ -1,10 +1,57 @@
 import json
 import os
 import subprocess
+import logging
+import itertools
 
+from enum import Enum
 from tempfile import TemporaryDirectory
 from typing import Dict, List, Union
 
+
+
+class ScatteringMethod(Enum):
+    DOT_PRODUCT = 'dot_product'
+    FLAT_CROSSPRODUCT = 'flat_crossproduct'
+    NESTED_CROSSPRODUCT = 'nested_crossproduct'
+
+# TODO: Move most of this logic to the go client
+def generate_io_graph_from_tool(tool_filepath, scattering_method=ScatteringMethod.DOT_PRODUCT, **kwargs):
+    # Open the file and load its content
+    with open(tool_filepath, 'r') as f:
+        tool = json.load(f)
+    
+    # Check if all kwargs are in the tool's inputs
+    for arg in kwargs:
+        if arg not in tool['inputs']:
+            logging.error(f'The argument {arg} is not in the tool inputs.')
+            logging.info(f'Available keys: {list(tool["inputs"].keys())}')
+            raise ValueError(f'The argument {arg} is not in the tool inputs.')
+    
+    # Scattering methods
+    if scattering_method == ScatteringMethod.DOT_PRODUCT:
+        if len(set(len(x) for x in kwargs.values())) != 1:
+            logging.error('All input arguments must have the same length for dot_product scattering method.')
+            raise ValueError('All input arguments must have the same length for dot_product scattering method.')
+        inputs_list = list(zip(*kwargs.values()))
+    elif scattering_method in [ScatteringMethod.FLAT_CROSSPRODUCT, ScatteringMethod.NESTED_CROSSPRODUCT]:
+        inputs_list = list(itertools.product(*kwargs.values()))
+    else:
+        logging.error(f'Invalid scattering method: {scattering_method}')
+        raise ValueError(f'Invalid scattering method: {scattering_method}')
+    
+    # Build the io_json_graph
+    io_json_graph = []
+    for inputs in inputs_list:
+        io_json_graph.append({
+            'tool': tool_filepath,
+            'inputs': {arg: {'class': tool['inputs'][arg]['type'], 'filepath': filepath} for arg, filepath in zip(kwargs.keys(), inputs)},
+            'outputs': {arg: {'class': tool['outputs'][arg]['type'], 'filepath': ''} for arg in tool['outputs']},
+            'state': 'created',
+            'errMsg': '',
+        })
+    
+    return io_json_graph
 
 def run_plex(io: Union[Dict, List[Dict]], concurrency=1, local=False, verbose=False, retry=False, plex_path="./plex"):
     if not (isinstance(io, dict) or (isinstance(io, list) and all(isinstance(i, dict) for i in io))):
