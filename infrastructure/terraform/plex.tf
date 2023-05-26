0c0d395ff89450473c3b85dc8df93f18d0d337e4
Humberto Evans <hevans66@gmail.com>
2023-05-26 14:07:19 -0700

    ansible compute work in progress

--- infrastructure/terraform/plex.tf
+++ infrastructure/terraform/plex.tf
@@ -18,6 +18,30 @@ resource "aws_instance" "plex_prod" {
   }
 }
 
+resource "aws_instance" "ops_test" {
+  for_each      = toset(["opstest"])
+  ami           = var.ami_main
+  instance_type = "g5.xlarge"
+
+  vpc_security_group_ids = [aws_security_group.plex.id]
+  key_name               = var.key_main
+  availability_zone      = var.availability_zones[0]
+
+  root_block_device {
+    volume_size = 1000
+    tags = {
+      Name = "plex-prod"
+    }
+  }
+
+  tags = {
+    Name        = "plex-prod-${each.key}"
+    InstanceKey = each.key
+    Type        = "compute"
+  }
+}
+
+
 resource "aws_eip" "plex_prod" {
   instance = aws_instance.plex_prod.id
   vpc      = true
