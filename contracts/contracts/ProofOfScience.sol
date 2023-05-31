02bbfbdc9d8c0f57a2277747d8e13ece4d1fd4dd
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-05-30 23:48:49 -0400

    removes Ownable modifier in nft contract

--- contracts/contracts/ProofOfScience.sol
+++ contracts/contracts/ProofOfScience.sol
@@ -2,16 +2,15 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 // todo: add CantBeEvil
 // import {LicenseVersion, CantBeEvil} from "@a16z/contracts/licenses/CantBeEvil.sol";
 
 /**
  * @title ProofOfScience
  * @dev This contract mints ERC-1155 tokens with IPFS URIs. 
- * Only the owner of the contract is allowed to mint new tokens.
+ * Any account is allowed to mint new tokens.
  */
-contract ProofOfScience is ERC1155, Ownable {
+contract ProofOfScience is ERC1155 {
     uint256 public tokenID = 0;
     mapping (uint256 => string) private _tokenURIs;
 
@@ -26,7 +25,6 @@ contract ProofOfScience is ERC1155, Ownable {
     /**
      * @dev Mints a new token and assigns it to `account`, 
      * increasing the total supply.
-     * Can only be called by the contract owner.
      *
      * @param account Recipient of the token minting.
      * @param tokenURI The IPFS URI of the associated token data.
@@ -36,7 +34,7 @@ contract ProofOfScience is ERC1155, Ownable {
      * - `account` cannot be the zero address.
      * - The token doesn't exist, `id` must not exist in other token's URI.
      */
-    function mint(address account, string memory tokenURI) public onlyOwner {
+    function mint(address account, string memory tokenURI) public {
         _mint(account, tokenID, 1, "");
         _setTokenURI(tokenID, tokenURI);
         tokenID = tokenID + 1;
@@ -60,17 +58,5 @@ contract ProofOfScience is ERC1155, Ownable {
      */
     function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
         _tokenURIs[tokenId] = _tokenURI;
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     * 
-     * Emits an {OwnershipTransferred} event.
-     *
-     * @param newOwner Address of the new owner
-     */
-    function transferContractOwnership(address newOwner) public onlyOwner {
-        transferOwnership(newOwner);
     }
 }
