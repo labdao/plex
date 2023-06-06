4b3479f7cbdcc04dfd21d0aa2c036a887189507e
Humberto Evans <humberto@labdao.com>
2023-06-06 13:15:25 -0700

    Move canary to compute box (#369)

---
+++ infrastructure/ansible/files/equibind_canary.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# exit the whole script if any command returns a non-zero exit code
+set -e
+
+# plex must run from the same place as tools directory
+cd {{ repo_dir }}
+
+plex -tool equibind -input-dir {{ repo_dir }}/testdata/binding/abl 2>&1 | tee plex_out.log
+# capture the exit status of the plex call
+plex_result_code=${PIPESTATUS[0]}
+# exit immediately if plex exited with an error
+if [ $plex_result_code -gt 0 ]; then
+  exit $plex_result_code
+fi
+
+# parse the output directory from the plex stdout
+result_dir=$(cat plex_out.log | grep 'Finished processing, results written to' | sed -n 's/^.*Finished processing, results written to //p' | sed 's/\/io.json//')
+
+# exit if no docked files are found
+cd "$result_dir/entry-0/outputs"
+if [ "$(find . -name '*docked.sdf' | grep 'docked.sdf')" == "" ]; then
+  echo "No docked files found"
+  exit 1
+else
+  echo "Docked files found"
+fi
+
+rm -rf $result_dir
+
+curl -X POST -H "Authorization: Bearer ${HEII_ON_CALL_API_KEY}" https://api.heiioncall.com./triggers/a7660c2f-5262-4392-918b-d98aee244890/checkin
+
+echo "Canary success"
