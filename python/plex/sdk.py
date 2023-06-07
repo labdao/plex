ee15a0f5d8e3e5f8e03016cf127bef7c0c8a4fe1
thetechnocrat-dev <josh.mcmenemy@openzyme.bio>
2023-06-07 16:12:43 -0400

    mint works

--- python/plex/sdk.py
+++ python/plex/sdk.py
@@ -69,8 +69,8 @@ def run_plex(io: Union[Dict, List[Dict]], concurrency=1, local=False, verbose=Fa
             json.dump(io, json_file, indent=4)
 
         cwd = os.getcwd()
-        plex_work_dir = os.environ.get("PLEX_WORK_DIR",os.path.dirname(os.path.dirname(cwd)))
-        cmd = [plex_path, "-input-io", json_file_path, "-concurrency", str(concurrency), "-output-dir", {outputDir}]
+        plex_work_dir = os.environ.get("PLEX_WORK_DIR", os.path.dirname(os.path.dirname(cwd)))
+        cmd = [plex_path, "-input-io", json_file_path, "-concurrency", str(concurrency), "-output-dir", outputDir]
 
         if local:
             cmd.append("-local=true")
@@ -113,17 +113,15 @@ def print_io_graph_status(io_graph):
     for state, count in state_count.items():
         print(f"IOs in {state} state: {count}")
 
-def mint_nft(io_json_path, web3=True, plex_path="./plex"):
+def mint_nft(io_json_path, plex_path="./plex"):
     # check if io_json_path is a valid file path
     if not os.path.isfile(io_json_path):
         raise ValueError('io_json_path must be a valid file path')
-    
+
     cwd = os.getcwd()
     plex_work_dir = os.environ.get("PLEX_WORK_DIR", os.path.dirname(os.path.dirname(cwd)))
-    cmd = [plex_path, "-input-io", io_json_path]
 
-    if web3:
-        cmd.append("-web3=true")
+    cmd = [plex_path, "-input-io", io_json_path, "-web3=true"]
 
     with subprocess.Popen(cmd, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True, cwd=plex_work_dir) as p:
         for line in p.stdout:
