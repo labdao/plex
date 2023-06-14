41b2e4992d07367a4f3c072b7a4d8900dc71ea1d
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-06-13 20:43:30 -0500

    Update nft.go

--- internal/web3/nft.go
+++ internal/web3/nft.go
@@ -208,11 +208,11 @@ func triggerMinting(recipientAddress, cid string) error {
 	}
 
 	if result.Status == "success" {
-		fmt.Println("\U0001F331\U0001F331\U0001F331\U0001F331\U0001F331") // 5 sapling emojis for open science
+		fmt.Println("\U0001F331\U0001F331\U0001F331\U0001F331\U0001F331") // 5 saplings for open science
 		fmt.Println("Minting process successful.")
 		fmt.Println("Thank you for making science more reproducible, open, and collaborative!")
 		fmt.Println("You can view your ProofOfScience NFT at https://testnets.opensea.io/.")
-		fmt.Println("\U0001F331\U0001F331\U0001F331\U0001F331\U0001F331") // 5 more sapling emojis for open science
+		fmt.Println("\U0001F331\U0001F331\U0001F331\U0001F331\U0001F331") // 5 more saplings for open science
 	} else {
 		fmt.Println("Minting process failed.")
 		fmt.Printf("Response from Autotask: %s\n", string(body))
