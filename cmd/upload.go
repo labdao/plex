cd2255b45bd2fd95c09bd989eab29cbc293fa614
thetechnocrat-dev <josh.mcmenemy@openzyme.bio>
2023-06-21 16:01:43 -0400

    added upload function

---
+++ cmd/upload.go
@@ -0,0 +1,52 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/labdao/plex/internal/ipfs"
+	"github.com/spf13/cobra"
+)
+
+var (
+	localPath string
+)
+
+var uploadCmd = &cobra.Command{
+	Use:   "upload",
+	Short: "Upload a file or directory to IPFS",
+	Long:  `Upload and pins a file or directory to IPFS. Will wrap single files in a directory before uploading. (50MB max). A`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cid, err := uploadPath(localPath)
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+		fmt.Println("Uploaded CID: ", cid)
+	},
+}
+
+func uploadPath(path string) (cid string, err error) {
+	info, err := os.Stat(path)
+
+	if os.IsNotExist(err) {
+		return "", err
+	}
+
+	if err != nil {
+		// Handle other possible errors here
+		return "", err
+	}
+
+	if info.IsDir() {
+		return ipfs.PinDir(path)
+	} else {
+		return ipfs.WrapAndPinFile(path)
+	}
+}
+
+func init() {
+	uploadCmd.Flags().StringVarP(&localPath, "path", "p", "", "Local file path to the file or dir to upload")
+
+	rootCmd.AddCommand(uploadCmd)
+}
