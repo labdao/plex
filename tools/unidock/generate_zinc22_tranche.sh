69d03fe65ccc7434fd2eefa71f8dca9f0f9c9174
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2023-07-05 18:54:32 +0200

    update tranche generator

---
+++ tools/unidock/generate_zinc22_tranche.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./tools/unidock/zinc22.sh tools/unidock/incomplete_index.wget "H04|H05|H06" E3
+./tools/unidock/zinc22.sh tools/unidock/incomplete_index.wget "H07|H08" E4
+./tools/unidock/zinc22.sh tools/unidock/incomplete_index.wget "H09|H10|H11" E5
+./tools/unidock/zinc22.sh tools/unidock/incomplete_index.wget "H12|H13" E6
--- tools/unidock/zinc22.sh
+++ tools/unidock/zinc22.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
-# This script works from an index of the ZINC22 database and downloads the pdbqt files of a specified tranche
-# The index is a text file with one line per file to download
-# The pdbqt files are downloaded to /outputs/pdbqt
-# The index of pdbqt files is written to /outputs/index.txt and can be used as input for docking tools
-
 # First argument is the file to read from
 FILE=$1
 
 # Second argument is the pattern to match
 PATTERN=$2
 
-# Third argument is the output directory
+# Third argument is the output zip name
 OUTPUT=$3
 
-# subset the index and download files
-grep "$PATTERN" "$FILE" | while read -r line; do
+
+# Subset the index and download files
+egrep "$PATTERN" "$FILE" | while read -r line; do
     echo "Downloading: $line"
     eval "$line"
 done
 
-# pull out the pdbqt files 
-mkdir -p $OUTPUT/tgz
-#mkdir -p $OUTPUT/$PATTERN
-mv zinc22/*/*/*/*/*.pdbqt.tgz $OUTPUT/tgz
+# Decompress tgz files directly in place
+find zinc22 -type f -name '*.pdbqt.tgz' -exec tar -xzvf {} -C zinc22/zinc-22d \;
 
-for file in $OUTPUT/tgz/*.tgz; do
-  # Decompress the file
-  tar -xzvf "$file" -C $OUTPUT/
-done
+# Search for all '.pdbqt' files and add them to a zip file
+find zinc22 -type f -name '*.pdbqt' -print | zip $OUTPUT.zip -@
 
-find $OUTPUT -type f -name '*.pdbqt' -print | zip $OUTPUT.zip -@
+# cleaning up
+rm -r zinc22
