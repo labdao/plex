c4724632a450fc861bc4eea6ed1953a2573c292a
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2023-07-05 19:51:31 +0200

    add example and inject tool

--- tools/unidock/generate_zinc22_tranche.sh
+++ tools/unidock/generate_zinc22_tranche.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # executing these steps in order makes sure that each larger set is a superset of each smaller set
+./tools/unidock/fetch_zinc22.sh tools/unidock/ZINC22-downloader-3D-complete-pdbqt.tgz.wget "H04" E2
 ./tools/unidock/fetch_zinc22.sh tools/unidock/ZINC22-downloader-3D-complete-pdbqt.tgz.wget "H04|H05|H06" E3
 ./tools/unidock/fetch_zinc22.sh tools/unidock/ZINC22-downloader-3D-complete-pdbqt.tgz.wget "H07|H08" E4
 ./tools/unidock/fetch_zinc22.sh tools/unidock/ZINC22-downloader-3D-complete-pdbqt.tgz.wget "H09|H10|H11" E5
