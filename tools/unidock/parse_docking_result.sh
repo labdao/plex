2a724818e39ebec7eae78c915476b8ec49ccb66b
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2023-07-05 20:14:45 +0200

    old bug in the parser

--- tools/unidock/parse_docking_result.sh
+++ tools/unidock/parse_docking_result.sh
@@ -7,18 +7,19 @@ input_dir=$1
 output_file=$2
 
 # Print CSV header
-echo "Filename,Ligand,Model,Affinity,rmsd l.b.,rmsd u.b.,inter_intra,inter,intra,unbound" > $output_file
+echo "filename,ligand,model,affinity,rmsd_lb,rmsd_ub,inter,intra,unbound" > $output_file
 
 # Scan through each PDBQT file in the directory
 for pdbqt_file in $input_dir/*.pdbqt; do
     ligand=$(basename $pdbqt_file .pdbqt)
     awk -v ligand="$ligand" -v file="$pdbqt_file" '
-        /REMARK  Name =/ {name=$4}
+        BEGIN {name="NA"}
         /MODEL/ {model=$2}
         /REMARK VINA RESULT/ {affinity=$4; rmsd_lb=$5; rmsd_ub=$6}
         /REMARK INTER \+ INTRA/ {inter_intra=$4}
         /REMARK INTER/ {inter=$3}
         /REMARK INTRA/ {intra=$3}
-        /REMARK UNBOUND/ {unbound=$3; print file","name","model","affinity","rmsd_lb","rmsd_ub","inter_intra","inter","intra","unbound}
+        /REMARK UNBOUND/ {unbound=$3}
+        /REMARK  Name =/ {name=$4; print file","name","model","affinity","rmsd_lb","rmsd_ub","inter","intra","unbound; name="NA"}
     ' $pdbqt_file >> $output_file
 done
