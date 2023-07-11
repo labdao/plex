ab32ff6dc7d08440c1bf5b1d0791f5a7f9e8a1b0
thetechnocrat-dev <josh.mcmenemy@openzyme.bio>
2023-07-11 16:25:32 -0400

    dev pip build will download go bin

--- python/setup.py
+++ python/setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
+from setuptools.command.develop import develop
 import os
 import subprocess
 import shutil
 import tempfile
+import sys
 
 
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        install.run(self)
-
+class PostSetupCommand:
+    """Base class for both installation and development mode."""
+    def run_setup(self):
         print("Running post-installation...")
 
         # Retrieve platform from environment variable
@@ -31,6 +31,10 @@ class PostInstallCommand(install):
                 with tempfile.TemporaryDirectory() as temp_dir:
                     self.download_and_extract(go_bin_url, temp_dir)
 
+                    # If self.install_scripts is None, use a default scripts directory
+                    if self.install_scripts is None:
+                        self.install_scripts = os.path.join(sys.prefix, 'bin')
+
                     # move the binary to the scripts installation directory
                     src = os.path.join(temp_dir, 'plex')
                     dst = os.path.join(self.install_scripts, 'plex')
@@ -42,7 +46,6 @@ class PostInstallCommand(install):
                     shutil.move(src, dst)
                     # set the binary as executable
                     os.chmod(dst, 0o755)
-
             except Exception as e:
                 print(f"Failed to download and extract the Go binary: {str(e)}")
                 raise
@@ -51,6 +54,19 @@ class PostInstallCommand(install):
         subprocess.run(f"curl -sSL {go_bin_url} | tar xvz -C {temp_dir}", shell=True, check=True)
 
 
+class PostInstallCommand(PostSetupCommand, install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        self.run_setup()
+
+
+class PostDevelopCommand(PostSetupCommand, develop):
+    """Post-installation for development mode."""
+    def run(self):
+        develop.run(self)
+        self.run_setup()
+
 setup(
     name="PlexLabExchange",
     version="0.8.11",
@@ -58,6 +74,7 @@ setup(
     package_dir={'': 'src'},  # tell setuptools that the packages are under the 'src' directory
     cmdclass={
         'install': PostInstallCommand,
+        'develop': PostDevelopCommand,
     },
     author="LabDAO",
     author_email="media@labdao.xyz",
