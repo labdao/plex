060c44ddf324edee14a3dafbffb6562d9031a0a8
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-08-28 19:31:22 +0200

    handlers modularized for readability in main func

--- gateway/app.go
+++ gateway/app.go
@@ -70,6 +70,117 @@ func sendJSONError(w http.ResponseWriter, message string, status int) {
 	json.NewEncoder(w).Encode(map[string]string{"message": message})
 }
 
+func healthCheckHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "Healthy")
+	}
+}
+
+func createUserHandler(db *gorm.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			sendJSONError(w, "Only POST method is supported", http.StatusBadRequest)
+			fmt.Println("Received non-POST request for /user endpoint.")
+			return
+		}
+
+		var requestData struct {
+			Username      string `json:"username"`
+			WalletAddress string `json:"walletAddress"`
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		if err := decoder.Decode(&requestData); err != nil {
+			sendJSONError(w, "Error parsing request body", http.StatusBadRequest)
+			fmt.Println("Error decoding request body:", err)
+			return
+		}
+
+		fmt.Printf("Received request to create user: Username: %s, WalletAddress: %s\n", requestData.Username, requestData.WalletAddress)
+
+		re := regexp.MustCompile(`^0x[0-9a-fA-F]{40}$`)
+		isValidAddress := re.MatchString(requestData.WalletAddress)
+		if !isValidAddress {
+			fmt.Printf("%s is not a valid Ethereum address\n", requestData.WalletAddress)
+			sendJSONError(w, "Invalid Ethereum address", http.StatusBadRequest)
+			return
+		}
+
+		var existingUser User
+		if err := db.Where("username = ? AND wallet_address = ?", requestData.Username, requestData.WalletAddress).First(&existingUser).Error; err != nil {
+			if err == gorm.ErrRecordNotFound {
+				// User does not exist, create new user
+				newUser := User{
+					Username:      requestData.Username,
+					WalletAddress: requestData.WalletAddress,
+				}
+				if result := db.Create(&newUser); result.Error != nil {
+					sendJSONError(w, fmt.Sprintf("Error creating user: %v", result.Error), http.StatusInternalServerError)
+					fmt.Println("Error creating user in database:", result.Error)
+					return
+				}
+				fmt.Printf("Successfully created user with ID: %d, Username: %s\n", newUser.ID, newUser.Username)
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusCreated)
+				json.NewEncoder(w).Encode(newUser)
+			} else {
+				// Some other error occurred during the query
+				sendJSONError(w, "Database error", http.StatusInternalServerError)
+				fmt.Println("Database error:", err)
+			}
+		} else {
+			// User with given username and wallet address already exists, return that user
+			fmt.Printf("User already exists with ID: %d, Username: %s\n", existingUser.ID, existingUser.Username)
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(existingUser)
+		}
+	}
+}
+
+func createDataFileHandler(db *gorm.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			sendJSONError(w, "Only POST method is supported", http.StatusBadRequest)
+			fmt.Println("Received non-POST request for /create-datafile endpoint.")
+			return
+		}
+
+		var requestData struct {
+			CID           string `json:"cid"`
+			WalletAddress string `json:"wallet_address"`
+			Filename      string `json:"filename"`
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		if err := decoder.Decode(&requestData); err != nil {
+			sendJSONError(w, "Error parsing request body", http.StatusBadRequest)
+			fmt.Println("Error decoding request body:", err)
+			return
+		}
+
+		fmt.Printf("Received request to create datafile: CID: %s, WalletAddress: %s, Filename: %s\n", requestData.CID, requestData.WalletAddress, requestData.Filename)
+
+		newFile := DataFile{
+			CID:           requestData.CID,
+			WalletAddress: requestData.WalletAddress,
+			Filename:      requestData.Filename,
+		}
+
+		if result := db.Create(&newFile); result.Error != nil {
+			sendJSONError(w, fmt.Sprintf("Error creating datafile: %v", result.Error), http.StatusInternalServerError)
+			fmt.Println("Error creating datafile in database:", result.Error)
+			return
+		}
+
+		fmt.Printf("Successfully created datafile with ID: %d, CID: %s\n", newFile.ID, newFile.CID)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+	}
+}
+
 func addToolHandler(db *gorm.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Received request at /add-tool")
@@ -164,105 +275,9 @@ func main() {
 		AllowedMethods:   []string{"GET", "POST"},
 	})
 
-	// Health check endpoint
-	http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, "Healthy")
-	})
-
-	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			sendJSONError(w, "Only POST method is supported", http.StatusBadRequest)
-			fmt.Println("Received non-POST request for /user endpoint.")
-			return
-		}
-
-		var requestData struct {
-			Username      string `json:"username"`
-			WalletAddress string `json:"walletAddress"`
-		}
-
-		decoder := json.NewDecoder(r.Body)
-		if err := decoder.Decode(&requestData); err != nil {
-			sendJSONError(w, "Error parsing request body", http.StatusBadRequest)
-			fmt.Println("Error decoding request body:", err)
-			return
-		}
-
-		fmt.Printf("Received request to create user: Username: %s, WalletAddress: %s\n", requestData.Username, requestData.WalletAddress)
-
-		// Regular expression to match Ethereum addresses
-		re := regexp.MustCompile(`^0x[0-9a-fA-F]{40}$`)
-		isValidAddress := re.MatchString(requestData.WalletAddress)
-		if !isValidAddress {
-			fmt.Printf("%s is not a valid Ethereum address\n", requestData.WalletAddress)
-			sendJSONError(w, "Invalid Ethereum address", http.StatusBadRequest)
-			return
-		}
-
-		var existingUser User
-		if err := db.Where("username = ? AND wallet_address = ?", requestData.Username, requestData.WalletAddress).First(&existingUser).Error; err != nil {
-			if err == gorm.ErrRecordNotFound {
-				// User does not exist, create new user
-				newUser := User{
-					Username:      requestData.Username,
-					WalletAddress: requestData.WalletAddress,
-				}
-				if result := db.Create(&newUser); result.Error != nil {
-					sendJSONError(w, fmt.Sprintf("Error creating user: %v", result.Error), http.StatusInternalServerError)
-					fmt.Println("Error creating user in database:", result.Error)
-					return
-				}
-				fmt.Printf("Successfully created user with ID: %d, Username: %s\n", newUser.ID, newUser.Username)
-				w.Header().Set("Content-Type", "application/json")
-				w.WriteHeader(http.StatusCreated)
-				json.NewEncoder(w).Encode(newUser)
-			} else {
-				// Some other error occurred during the query
-				sendJSONError(w, "Database error", http.StatusInternalServerError)
-				fmt.Println("Database error:", err)
-			}
-		} else {
-			// User with given username and wallet address already exists, return that user
-			fmt.Printf("User already exists with ID: %d, Username: %s\n", existingUser.ID, existingUser.Username)
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(existingUser)
-		}
-	})
-
-	http.HandleFunc("/create-datafile", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "Only POST method is supported", http.StatusBadRequest)
-			return
-		}
-
-		var requestData struct {
-			CID           string `json:"cid"`
-			WalletAddress string `json:"wallet_address"`
-			Filename      string `json:"filename"`
-		}
-
-		decoder := json.NewDecoder(r.Body)
-		if err := decoder.Decode(&requestData); err != nil {
-			http.Error(w, "Error parsing request body", http.StatusBadRequest)
-			return
-		}
-
-		newFile := DataFile{
-			CID:           requestData.CID,
-			WalletAddress: requestData.WalletAddress,
-			Filename:      requestData.Filename,
-		}
-		if result := db.Create(&newFile); result.Error != nil {
-			http.Error(w, fmt.Sprintf("Error creating datafile: %v", result.Error), http.StatusInternalServerError)
-			return
-		}
-
-		w.WriteHeader(http.StatusCreated)
-		fmt.Fprint(w, "DataFile created successfully!")
-	})
-
+	http.HandleFunc("/healthcheck", healthCheckHandler())
+	http.HandleFunc("/user", createUserHandler(db))
+	http.HandleFunc("/create-datafile", createDataFileHandler(db))
 	http.HandleFunc("/add-tool", addToolHandler(db))
 
 	// Start the server with CORS middleware
