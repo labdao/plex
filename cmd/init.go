6bc8ab6fe9d3817085efddf7d391607389d1f31e
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-08-30 18:02:53 +0200

    initialize io refactored

--- cmd/init.go
+++ cmd/init.go
@@ -34,7 +34,7 @@ var initCmd = &cobra.Command{
 			log.Fatal("Invalid inputs JSON:", err)
 		}
 
-		ioJson, err := InitilizeIo(toolPath, scatteringMethod, kwargs)
+		ioJson, err := InitializeIo(toolPath, scatteringMethod, kwargs)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -72,75 +72,32 @@ var initCmd = &cobra.Command{
 	},
 }
 
-func InitilizeIo(toolPath string, scatteringMethod string, inputVectors map[string][]string) ([]ipwl.IO, error) {
+func InitializeIo(toolPath string, scatteringMethod string, inputVectors map[string][]string) ([]ipwl.IO, error) {
 	// Open the file and load its content
 	tool, toolInfo, err := ipwl.ReadToolConfig(toolPath)
 	if err != nil {
 		return nil, err
 	}
 
-	// Check if all kwargs are in the tool's inputs
-	for inputKey := range inputVectors {
-		if _, exists := tool.Inputs[inputKey]; !exists {
-			log.Printf("The argument %s is not in the tool inputs.\n", inputKey)
-			log.Printf("Available keys: %v\n", tool.Inputs)
-			return nil, fmt.Errorf("the argument %s is not in the tool inputs", inputKey)
-		}
+	// Validate input keys
+	err = validateInputKeys(inputVectors, tool.Inputs)
+	if err != nil {
+		return nil, err
 	}
 
-	// Handle scattering methods and create the inputsList
+	// Handle scattering methods and create the ist
 	var inputsList [][]string
 	switch scatteringMethod {
 	case "dotProduct":
-		// check if all lists have the same length
-		var vectorLength int
-		for _, v := range inputVectors {
-			if vectorLength == 0 {
-				vectorLength = len(v)
-				continue
-			}
-			if len(v) != vectorLength {
-				return nil, fmt.Errorf("all input arguments must have the same length for dot_product scattering method")
-			}
-			vectorLength = len(v)
-		}
-		for i := 0; i < vectorLength; i++ {
-			tmp := []string{}
-			for _, v := range inputVectors {
-				tmp = append(tmp, v[i])
-			}
-			inputsList = append(inputsList, tmp)
+		inputsList, err = dotProductScattering(inputVectors)
+		if err != nil {
+			return nil, err
 		}
 	case "crossProduct":
-		cartesian := func(arrs ...[]string) [][]string {
-			result := [][]string{{}}
-
-			for _, arr := range arrs {
-				var temp [][]string
-
-				for _, res := range result {
-					for _, str := range arr {
-						product := append([]string{}, res...)
-						product = append(product, str)
-						temp = append(temp, product)
-					}
-				}
-
-				result = temp
-			}
-
-			return result
+		inputsList, err = crossProductScattering(inputVectors)
+		if err != nil {
+			return nil, err
 		}
-
-		keys := make([]string, 0, len(inputVectors))
-		for k := range inputVectors {
-			keys = append(keys, k)
-		}
-		arrays := make([][]string, len(inputVectors))
-		for i, k := range keys {
-			arrays[i] = inputVectors[k]
-		}
-		inputsList = cartesian(arrays...)
 	default:
 		return nil, fmt.Errorf("invalid scattering method: %s", scatteringMethod)
 	}
@@ -154,62 +111,136 @@ func InitilizeIo(toolPath string, scatteringMethod string, inputVectors map[stri
 		userId = ""
 	}
 
-	// populate ioJSONGraph based on inputsList
-	var ioJSONGraph []ipwl.IO
+	var ioList []ipwl.IO
+
 	for _, inputs := range inputsList {
-		io := ipwl.IO{
-			Tool:    toolInfo,
-			Inputs:  make(map[string]ipwl.FileInput),
-			Outputs: make(map[string]ipwl.Output),
-			State:   "created",
-			ErrMsg:  "",
-			UserID:  userId,
+		io, err := createSingleIo(inputs, tool, toolInfo, userId, inputVectors)
+		if err != nil {
+			return nil, err
 		}
-
-		inputKeys := make([]string, 0, len(inputVectors))
-		for k := range inputVectors {
-			inputKeys = append(inputKeys, k)
-		}
-
-		for i, inputValue := range inputs {
-			inputKey := inputKeys[i]
-
-			if strings.Count(inputValue, "/") == 1 {
-				parts := strings.Split(inputValue, "/")
-				cid := parts[0]
-				fileName := parts[1]
-				if !ipfs.IsValidCID(cid) {
-					return nil, fmt.Errorf("invalid CID: %s", cid)
-				}
-				io.Inputs[inputKey] = ipwl.FileInput{
-					Class:    tool.Inputs[inputKey].Type,
-					FilePath: fileName,
-					IPFS:     cid,
-				}
-			} else {
-				cid, err := ipfs.WrapAndPinFile(inputValue) // Pin the file and get the CID
-				if err != nil {
-					return nil, err
-				}
-				io.Inputs[inputKey] = ipwl.FileInput{
-					Class:    tool.Inputs[inputKey].Type,
-					FilePath: filepath.Base(inputValue), // Use the respective input value from inputsList
-					IPFS:     cid,                       // Use the CID returned by WrapAndPinFile
-				}
-			}
-		}
-
-		for outputKey := range tool.Outputs {
-			io.Outputs[outputKey] = ipwl.FileOutput{
-				Class:    tool.Outputs[outputKey].Type,
-				FilePath: "", // Assuming filepath is empty, adapt as needed
-				IPFS:     "", // Assuming IPFS is not provided, adapt as needed
-			}
-		}
-		ioJSONGraph = append(ioJSONGraph, io)
+		ioList = append(ioList, io)
 	}
 
-	return ioJSONGraph, nil
+	return ioList, nil
+}
+
+func validateInputKeys(inputVectors map[string][]string, toolInputs map[string]ipwl.ToolInput) error {
+	for inputKey := range inputVectors {
+		if _, exists := toolInputs[inputKey]; !exists {
+			log.Printf("The argument %s is not in the tool inputs.\n", inputKey)
+			log.Printf("Available keys: %v\n", toolInputs)
+			return fmt.Errorf("the argument %s is not in the tool inputs", inputKey)
+		}
+	}
+	return nil
+}
+
+func dotProductScattering(inputVectors map[string][]string) ([][]string, error) {
+	var vectorLength int
+	for _, v := range inputVectors {
+		if vectorLength == 0 {
+			vectorLength = len(v)
+			continue
+		}
+		if len(v) != vectorLength {
+			return nil, fmt.Errorf("all input arguments must have the same length for dotProduct scattering method")
+		}
+	}
+
+	var inputsList [][]string
+	for i := 0; i < vectorLength; i++ {
+		tmp := []string{}
+		for _, v := range inputVectors {
+			tmp = append(tmp, v[i])
+		}
+		inputsList = append(inputsList, tmp)
+	}
+
+	return inputsList, nil
+}
+
+func crossProductScattering(inputVectors map[string][]string) ([][]string, error) {
+	cartesian := func(arrs ...[]string) [][]string {
+		result := [][]string{{}}
+		for _, arr := range arrs {
+			var temp [][]string
+			for _, res := range result {
+				for _, str := range arr {
+					product := append([]string{}, res...)
+					product = append(product, str)
+					temp = append(temp, product)
+				}
+			}
+			result = temp
+		}
+		return result
+	}
+
+	keys := make([]string, 0, len(inputVectors))
+	for k := range inputVectors {
+		keys = append(keys, k)
+	}
+	arrays := make([][]string, len(inputVectors))
+	for i, k := range keys {
+		arrays[i] = inputVectors[k]
+	}
+	inputsList := cartesian(arrays...)
+
+	return inputsList, nil
+}
+
+func createSingleIo(inputs []string, tool ipwl.Tool, toolInfo ipwl.ToolInfo, userId string, inputVectors map[string][]string) (ipwl.IO, error) {
+	io := ipwl.IO{
+		Tool:    toolInfo,
+		Inputs:  make(map[string]ipwl.FileInput),
+		Outputs: make(map[string]ipwl.Output),
+		State:   "created",
+		ErrMsg:  "",
+		UserID:  userId,
+	}
+
+	inputKeys := make([]string, 0, len(inputVectors))
+	for k := range inputVectors {
+		inputKeys = append(inputKeys, k)
+	}
+
+	for i, inputValue := range inputs {
+		inputKey := inputKeys[i]
+
+		if strings.Count(inputValue, "/") == 1 {
+			parts := strings.Split(inputValue, "/")
+			cid := parts[0]
+			fileName := parts[1]
+			if !ipfs.IsValidCID(cid) {
+				return io, fmt.Errorf("invalid CID: %s", cid)
+			}
+			io.Inputs[inputKey] = ipwl.FileInput{
+				Class:    tool.Inputs[inputKey].Type,
+				FilePath: fileName,
+				IPFS:     cid,
+			}
+		} else {
+			cid, err := ipfs.WrapAndPinFile(inputValue)
+			if err != nil {
+				return io, err
+			}
+			io.Inputs[inputKey] = ipwl.FileInput{
+				Class:    tool.Inputs[inputKey].Type,
+				FilePath: filepath.Base(inputValue),
+				IPFS:     cid,
+			}
+		}
+	}
+
+	for outputKey, outputValue := range tool.Outputs {
+		io.Outputs[outputKey] = ipwl.FileOutput{
+			Class:    outputValue.Type,
+			FilePath: "",
+			IPFS:     "",
+		}
+	}
+
+	return io, nil
 }
 
 func init() {
