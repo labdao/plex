9cc1c15c8b5024d90088ec581fcfbbbb3eee4876
alabdao <139594838+alabdao@users.noreply.github.com>
2023-08-31 11:40:11 -0400

    [LAB-577] Plex upgrade to 0.10.0
    Also add `-a` for append to `tee`
    
    Added `-a` in `grep` to ignore binary error
    
    Custom IPFS Port setup to not conflict with any running IPFS process

--- infrastructure/ansible/files/colabfold_mini_canary.sh
+++ infrastructure/ansible/files/colabfold_mini_canary.sh
@@ -3,10 +3,22 @@
 # exit the whole script if any command returns a non-zero exit code
 set -e
 
+# IFPS Custom setup
+export IPFS_PATH=$(mktemp -d)
+export BACALHAU_SERVE_IPFS_PATH=${IPFS_PATH}
+
+# Ensure there is no port conflict with other canaries
+ipfs init -e
+ipfs config Addresses.API /ip4/127.0.0.1/tcp/8001
+ipfs config Addresses.Gateway /ip4/127.0.0.1/tcp/8002
+ipfs config Addresses.Swarm --json '["/ip4/0.0.0.0/tcp/8000"]'
+
 # plex must run from the same place as tools directory
 cd {{ repo_dir }}
 
-plex init -t {{ repo_dir }}/tools/colabfold-mini.json -i '{"sequence": ["{{ repo_dir }}/testdata/folding/test.fasta"]}' --scatteringMethod=dotProduct --autoRun=true -a test -a cron | tee plex_colabfold_out.log
+echo "$(date) - Running Canary" | tee -a plex_colabfold_out.log
+
+plex init -t {{ repo_dir }}/tools/colabfold-mini.json -i '{"sequence": ["{{ repo_dir }}/testdata/folding/test.fasta"]}' --scatteringMethod=dotProduct --autoRun=true -a test -a cron | tee -a plex_colabfold_out.log
 # capture the exit status of the plex call
 plex_result_code=${PIPESTATUS[0]}
 # exit immediately if plex exited with an error
@@ -15,7 +27,7 @@ if [ $plex_result_code -gt 0 ]; then
 fi
 
 # parse the output directory from the plex stdout
-result_dir=$(cat plex_colabfold_out.log | grep 'Finished processing, results written to' | sed -n 's/^.*Finished processing, results written to //p' | sed 's/\/io.json//')
+result_dir=$(cat plex_colabfold_out.log | grep -a 'Finished processing, results written to' | sed -n 's/^.*Finished processing, results written to //p' | sed 's/\/io.json//' | tail -n 1)
 
 # exit if no docked files are found
 cd "$result_dir/entry-0/outputs"
@@ -30,4 +42,6 @@ rm -rf $result_dir
 
 curl -X POST -H "Authorization: Bearer ${HEII_ON_CALL_API_KEY}" https://api.heiioncall.com./triggers/991a6388-5c61-422c-b8cf-202b4c4b55a6/checkin
 
-echo "Colabfold mini canary success"
+echo "$(date) - Colabfold mini canary success" | tee -a plex_colabfold_out.log
+
+rm -rf ${IPFS_PATH}
--- infrastructure/ansible/files/equibind_canary.sh
+++ infrastructure/ansible/files/equibind_canary.sh
@@ -3,10 +3,22 @@
 # exit the whole script if any command returns a non-zero exit code
 set -e
 
+# IFPS Custom setup
+export IPFS_PATH=$(mktemp -d)
+export BACALHAU_SERVE_IPFS_PATH=${IPFS_PATH}
+
+# Ensure there is no port conflict with other canaries
+ipfs init -e
+ipfs config Addresses.API /ip4/127.0.0.1/tcp/7001
+ipfs config Addresses.Gateway /ip4/127.0.0.1/tcp/7002
+ipfs config Addresses.Swarm --json '["/ip4/0.0.0.0/tcp/7000"]'
+
 # plex must run from the same place as tools directory
 cd {{ repo_dir }}
 
-plex init -t {{ repo_dir }}/tools/equibind.json -i '{"protein": ["{{ repo_dir }}/testdata/binding/abl/7n9g.pdb"], "small_molecule": ["{{ repo_dir }}/testdata/binding/abl/ZINC000003986735.sdf"]}' --scatteringMethod=dotProduct -a test -a cron --autoRun=true 2>&1 | tee plex_equibind_out.log
+echo "$(date) - Running Canary" | tee -a plex_equibind_out.log
+
+plex init -t {{ repo_dir }}/tools/equibind.json -i '{"protein": ["{{ repo_dir }}/testdata/binding/abl/7n9g.pdb"], "small_molecule": ["{{ repo_dir }}/testdata/binding/abl/ZINC000003986735.sdf"]}' --scatteringMethod=dotProduct -a test -a cron --autoRun=true 2>&1 | tee -a plex_equibind_out.log
 # capture the exit status of the plex call
 plex_result_code=${PIPESTATUS[0]}
 # exit immediately if plex exited with an error
@@ -15,7 +27,7 @@ if [ $plex_result_code -gt 0 ]; then
 fi
 
 # parse the output directory from the plex stdout
-result_dir=$(cat plex_equibind_out.log | grep 'Finished processing, results written to' | sed -n 's/^.*Finished processing, results written to //p' | sed 's/\/io.json//')
+result_dir=$(cat plex_equibind_out.log | grep -a 'Finished processing, results written to' | sed -n 's/^.*Finished processing, results written to //p' | sed 's/\/io.json//' | tail -n 1)
 
 # exit if no docked files are found
 cd "$result_dir/entry-0/outputs"
@@ -30,4 +42,6 @@ rm -rf $result_dir
 
 curl -X POST -H "Authorization: Bearer ${HEII_ON_CALL_API_KEY}" https://api.heiioncall.com./triggers/a7660c2f-5262-4392-918b-d98aee244890/checkin
 
-echo "Canary success"
+echo "$(date) - Canary success" | tee -a plex_equibind_out.log
+
+rm -rf ${IPFS_PATH}
