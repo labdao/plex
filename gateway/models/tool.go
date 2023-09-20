983b6344e98cf2e68e7436be317ab98acacb40c5
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-09-20 16:16:42 -0400

    user tool and datafile handlers updated for new model structs

--- gateway/models/tool.go
+++ gateway/models/tool.go
@@ -1,8 +1,7 @@
 package models
 
 type Tool struct {
-	ID            uint   `gorm:"primaryKey"`
-	CID           string `gorm:"column:cid;type:varchar(255);not null"` // column name specified to avoid GORM default snake case
-	ToolJSON      string `gorm:"type:varchar(255);not null"`
+	CID           string `gorm:"primaryKey;column:cid;type:varchar(255);not null"` // column name specified to avoid GORM default snake case
+	ToolJSON      string `gorm:"type:text;not null"`
 	WalletAddress string `gorm:"type:varchar(42);not null"`
 }
