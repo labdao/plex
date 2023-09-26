5f4aef163d52d69732cb207433c46bd2ef4c7d54
thetechnocrat-dev <josh.mcmenemy@openzyme.bio>
2023-09-26 08:05:15 -0400

    fix user view

--- gateway/models/user.go
+++ gateway/models/user.go
@@ -1,10 +1,6 @@
 package models
 
-// gorm supports the use of composite primary keys
-// combination of wallet address and email must be unique
-// https://gorm.io/docs/composite_primary_key.html
-
 type User struct {
-	WalletAddress string `gorm:"primaryKey;type:varchar(42);not null" json:"walletAddress"`
-	Email         string `gorm:"primaryKey;not null" json:"email"`
+	Username      string `json:"username"`
+	WalletAddress string `gorm:"type:varchar(42);not null" json:"walletAddress"`
 }
