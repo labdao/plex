eba0cf69606076b56cfa6e02b006906bad0d6492
The Technocrat <josh.mcmenemy@openzyme.bio>
2023-09-29 01:19:48 -0700

    Revert "678 lab 641 add run job view (#680)"
    This reverts commit a155dbd4dc8b3f082c17b375985c89458a00c473.

--- gateway/models/tool.go
+++ gateway/models/tool.go
@@ -1,12 +1,7 @@
 package models
 
-import (
-	"gorm.io/datatypes"
-)
-
 type Tool struct {
-	CID           string         `gorm:"primaryKey;column:cid;type:varchar(255);not null"` // column name specified to avoid GORM default snake case
-	Name          string         `gorm:"type:text;not null;unique"`
-	WalletAddress string         `gorm:"type:varchar(42);not null"`
-	ToolJson      datatypes.JSON `gorm:"type:json"`
+	CID           string `gorm:"primaryKey;column:cid;type:varchar(255);not null"` // column name specified to avoid GORM default snake case
+	Name          string `gorm:"type:text;not null;unique"`
+	WalletAddress string `gorm:"type:varchar(42);not null"`
 }
