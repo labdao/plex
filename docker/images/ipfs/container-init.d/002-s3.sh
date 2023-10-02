5d715ea4a79e06b1e7d4f5999f425aeb03a839a8
alabdao <139594838+alabdao@users.noreply.github.com>
2023-10-02 09:51:04 -0400

    wip

--- docker/images/ipfs/container-init.d/002-s3.sh
+++ docker/images/ipfs/container-init.d/002-s3.sh
@@ -7,15 +7,15 @@ fi
 
 if [ "${IPFS_S3_ENABLED}" == "true" ]; then
   echo "IPFS PATH: ${IPFS_PATH}"
-  
+
   # Check if running in ECS, set credentialsEndpoint
   if [ "${AWS_EXECUTION_ENV}" == "AWS_ECS_FARGATE" ]; then
     export CLUSTER_CREDENTIALS_ENDPOINT=http://169.254.170.2${AWS_CONTAINER_CREDENTIALS_RELATIVE_URI}
   fi
-  
+
   # We backup old config file
   cp "${IPFS_PATH}"/config "${IPFS_PATH}"/config_bak
-  
+
   # We inject the S3 plugin datastore
   # Important: Make sure your fill out the optionnal parameters $CLUSTER_S3_BUCKET, $CLUSTER_AWS_KEY, $CLUSTER_AWS_SECRET in the cloudformation parameters
   cat "${IPFS_PATH}"/config_bak | \
@@ -49,7 +49,7 @@ if [ "${IPFS_S3_ENABLED}" == "true" ]; then
       ],
       type: \"mount\"
   }" > "${IPFS_PATH}"/config
-  
+
   # We override the ${IPFS_PATH}/datastore_spec file
   echo "{\"mounts\":[{\"bucket\":\"${CLUSTER_S3_BUCKET}\",\"mountpoint\":\"/blocks\",\"region\":\"${AWS_REGION}\",\"rootDirectory\":\"${CLUSTER_PEERNAME}\"},{\"mountpoint\":\"/\",\"path\":\"datastore\",\"type\":\"levelds\"}],\"type\":\"mount\"}" > "${IPFS_PATH}"/datastore_spec
 fi
