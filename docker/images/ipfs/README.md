d30b8c4c411c2751b81e8d7ec6c9eb60c2cec7a9
alabdao <139594838+alabdao@users.noreply.github.com>
2023-10-02 16:08:03 -0400

    [LAB-636] ci setup (#679)
    Co-authored-by: thetechnocrat-dev <josh.mcmenemy@openzyme.bio>

---
+++ docker/images/ipfs/README.md
@@ -0,0 +1,1 @@
+Copied mostly from https://github.com/ipfs/go-ds-s3/tree/master/docker
---
+++ docker/images/ipfs/container-init.d/004-swarm-base64-key.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+user=ipfs
+repo="$IPFS_PATH"
+
+if [ "${IPFS_DEBUG}" == "true" ]; then
+  set -x
+fi
+
+
+# Set up the swarm key, if provided
+SWARM_KEY_FILE="$repo/swarm.key"
+SWARM_KEY_PERM=0600
+
+# Create a swarm key from a given environment variable
+ls -ltra "$SWARM_KEY_FILE" || true
+
+if [ -n "$IPFS_SWARM_KEY_BASE64" ] && [ "${PRIVATE_IPFS}" == "true" ]; then
+  echo "Copying swarm key from variable IPFS_SWARM_KEY_BASE64..."
+  echo "$IPFS_SWARM_KEY_BASE64" | base64 -d >"$SWARM_KEY_FILE" || exit 1
+  chmod $SWARM_KEY_PERM "$SWARM_KEY_FILE"
+fi
