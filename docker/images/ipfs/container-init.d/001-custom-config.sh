7dc02333bc4d06cbf8eb7fd09bcd074098a4db7e
alabdao <139594838+alabdao@users.noreply.github.com>
2023-10-02 16:08:03 -0400

    [LAB-636] ci setup (#679)
    Co-authored-by: thetechnocrat-dev <josh.mcmenemy@openzyme.bio>

---
+++ docker/images/ipfs/container-init.d/001-custom-config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+if [ "${IPFS_DEBUG}" == "true" ]; then
+  set -x
+fi
+
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST", "GET"]'
+ipfs config Pinning.Recursive true
+ipfs config --json Swarm.RelayClient.Enabled false
