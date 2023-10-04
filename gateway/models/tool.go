a4c521fc433ae90f1144417344b8395a81c69f0d
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-10-04 03:12:16 -0400

    correct tool and user models

--- gateway/models/tool.go
+++ gateway/models/tool.go
@@ -2,10 +2,6 @@ package models
 
 type Tool struct {
 	CID           string `gorm:"primaryKey;column:cid;type:varchar(255);not null"` // column name specified to avoid GORM default snake case
-<<<<<<< HEAD
-	ToolJSON      string `gorm:"type:text"`
-=======
 	Name          string `gorm:"type:text;not null;unique"`
->>>>>>> main
 	WalletAddress string `gorm:"type:varchar(42);not null"`
 }
--- gateway/models/user.go
+++ gateway/models/user.go
@@ -1,11 +1,6 @@
 package models
 
 type User struct {
-<<<<<<< HEAD
 	WalletAddress string `gorm:"primaryKey;type:varchar(42);not null" json:"walletAddress"`
 	EmailAddress  string `gorm:"type:text" json:"emailAddress"`
-=======
-	Username      string `json:"username"`
-	WalletAddress string `gorm:"type:varchar(42);not null" json:"walletAddress"`
->>>>>>> main
 }
