796da59681a5fee64418b2d466cd88ecd3a66d4f
alabdao <139594838+alabdao@users.noreply.github.com>
2023-10-12 15:25:34 -0400

    updates

--- docker/images/backend/docker-entrypoint.sh
+++ docker/images/backend/docker-entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 set -e
 
+repo="$IPFS_PATH"
+
+if [ -e "$repo/config" ]; then
+  echo "Found IPFS fs-repo at $repo"
+else
+  ipfs init ${IPFS_PROFILE:+"--profile=$IPFS_PROFILE"}
+  ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
+  ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
+fi
+
+
 find /container-init.d -maxdepth 1 -type f -iname '*.sh' -print0 | sort -z | xargs -n 1 -0 -r container_init_run
 
 exec /plex "$@"
