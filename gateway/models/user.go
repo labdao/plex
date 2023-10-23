5d822ad8f5ba8c61f270bb20645882ca7c10fc5f
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-10-23 13:09:44 -0400

    user model now includes membership status bool

--- gateway/models/user.go
+++ gateway/models/user.go
@@ -2,4 +2,5 @@ package models
 
 type User struct {
 	WalletAddress string `gorm:"primaryKey;type:varchar(42);not null" json:"walletAddress"`
+	IsMember      bool   `gorm:"type:boolean;not null;default:false" json:"isMember"`
 }
