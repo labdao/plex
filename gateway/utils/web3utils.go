6a71d452e13e64ce41abccf33295afc14454daf4
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-10-27 14:02:03 -0400

    backend correctly determines if wallet holds record and updates in users table

--- gateway/utils/web3utils.go
+++ gateway/utils/web3utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 	"math/big"
 	"os"
@@ -14,20 +15,20 @@ import (
 )
 
 func getContractABI() (*abi.ABI, error) {
-	file, err := os.Open("../../contracts/artifacts/build-info/a7fa49d6da0732841f00ec7c540205a6.json")
+	file, err := os.Open("gateway/utils/clean-abi.json")
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error opening ABI file: %w", err)
 	}
 	defer file.Close()
 
 	byteValue, err := ioutil.ReadAll(file)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error reading ABI file: %w", err)
 	}
 
 	parsedABI, err := abi.JSON(strings.NewReader(string(byteValue)))
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error parsing ABI: %w", err)
 	}
 
 	return &parsedABI, nil
@@ -36,22 +37,21 @@ func getContractABI() (*abi.ABI, error) {
 func CheckNFTOwnership(walletAddress string) (bool, error) {
 	parsedABI, err := getContractABI()
 	if err != nil {
-		return false, err
+		return false, fmt.Errorf("error getting contract ABI: %w", err)
 	}
 
-	ethClient := os.Getenv("OPTIMISM_GOERLI_ALCHEMY_RPC")
+	ethClient := os.Getenv("NEXT_PUBLIC_OPTIMISM_GOERLI_ALCHEMY_RPC")
 	client, err := ethclient.Dial(ethClient)
 	if err != nil {
-		return false, err
+		return false, fmt.Errorf("error dialing ethClient: %w", err)
 	}
 
 	contractAddress := common.HexToAddress("0xda70C0709d4213eE8441E4731A5F662C0406ed7e")
 	address := common.HexToAddress(walletAddress)
 
-	// get maximum token ID
 	callData, err := parsedABI.Pack("tokenID")
 	if err != nil {
-		return false, err
+		return false, fmt.Errorf("error packing tokenID call: %w", err)
 	}
 
 	msg := ethereum.CallMsg{
@@ -61,33 +61,25 @@ func CheckNFTOwnership(walletAddress string) (bool, error) {
 
 	result, err := client.CallContract(context.Background(), msg, nil)
 	if err != nil {
-		return false, err
+		return false, fmt.Errorf("error calling contract for tokenID: %w", err)
 	}
 
-	maxTokenID := new(big.Int)
-	err = parsedABI.UnpackIntoInterface(maxTokenID, "tokenID", result)
-	if err != nil {
-		return false, err
-	}
+	maxTokenID := new(big.Int).SetBytes(result)
 
 	for tokenID := big.NewInt(0); tokenID.Cmp(maxTokenID) <= 0; tokenID.Add(tokenID, big.NewInt(1)) {
 		callData, err := parsedABI.Pack("balanceOf", address, tokenID)
 		if err != nil {
-			return false, err
+			return false, fmt.Errorf("error packing balanceOf call for tokenID %s: %w", tokenID.String(), err)
 		}
 
 		msg.Data = callData
 
 		result, err := client.CallContract(context.Background(), msg, nil)
 		if err != nil {
-			return false, err
+			return false, fmt.Errorf("error calling contract for balanceOf with tokenID %s: %w", tokenID.String(), err)
 		}
 
-		balance := new(big.Int)
-		err = parsedABI.UnpackIntoInterface(balance, "balanceOf", result)
-		if err != nil {
-			return false, err
-		}
+		balance := new(big.Int).SetBytes(result)
 
 		if balance.Cmp(big.NewInt(0)) > 0 {
 			return true, nil
