24ba625b29818166122d73f17876d1246afe9d5c
thetechnocrat-dev <josh.mcmenemy@openzyme.bio>
2023-10-30 16:30:35 +0000

    create initial migration that matches current schema

---
+++ gateway/migrations/1_initial_schema.down.sql
@@ -0,0 +1,23 @@
+-- Drop many-to-many relation tables
+DROP TABLE IF EXISTS job_inputs;
+DROP TABLE IF EXISTS job_outputs;
+
+-- Drop users table
+DROP TABLE IF EXISTS users;
+
+-- Drop tools table
+DROP TABLE IF EXISTS tools;
+
+-- Drop jobs table along with its indexes
+DROP INDEX IF EXISTS idx_jobs_tool_id;
+DROP INDEX IF EXISTS idx_jobs_flow_id;
+DROP TABLE IF EXISTS jobs;
+
+-- Drop input_outputs table
+DROP TABLE IF EXISTS input_outputs;
+
+-- Drop flows table
+DROP TABLE IF EXISTS flows;
+
+-- Drop data_files table
+DROP TABLE IF EXISTS data_files;
---
+++ gateway/migrations/1_initial_schema.up.sql
@@ -0,0 +1,66 @@
+-- Create data_files table
+CREATE TABLE data_files (
+    cid VARCHAR(255) NOT NULL PRIMARY KEY,
+    wallet_address VARCHAR(42) NOT NULL,
+    filename VARCHAR(255) NOT NULL,
+    timestamp TIMESTAMP
+);
+
+-- Create flows table
+CREATE TABLE flows (
+    cid VARCHAR(255) NOT NULL PRIMARY KEY,
+    name VARCHAR(255),
+    wallet_address VARCHAR(42) NOT NULL
+);
+
+-- Create input_outputs table
+CREATE TABLE input_outputs (
+    id SERIAL PRIMARY KEY,
+    job_id INT NOT NULL,
+    key_name VARCHAR(255) NOT NULL,
+    class VARCHAR(255) NOT NULL,
+    datafile_id INT NOT NULL
+);
+
+-- Create jobs table
+CREATE TABLE jobs (
+    bacalhau_job_id VARCHAR(255) NOT NULL PRIMARY KEY,
+    state VARCHAR(255) DEFAULT 'processing',
+    error TEXT DEFAULT '',
+    wallet_address VARCHAR(255),
+    tool_id VARCHAR(255) NOT NULL,
+    flow_id VARCHAR(255) NOT NULL
+);
+
+CREATE INDEX idx_jobs_tool_id ON jobs(tool_id);
+CREATE INDEX idx_jobs_flow_id ON jobs(flow_id);
+
+-- Create tools table
+CREATE TABLE tools (
+    cid VARCHAR(255) NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    wallet_address VARCHAR(42) NOT NULL,
+    tool_json JSON
+);
+
+-- Create users table
+CREATE TABLE users (
+    wallet_address VARCHAR(42) NOT NULL PRIMARY KEY
+);
+
+-- Create many-to-many relation tables
+CREATE TABLE job_inputs (
+    bacalhau_job_id VARCHAR(255) NOT NULL,
+    cid VARCHAR(255) NOT NULL,
+    PRIMARY KEY (bacalhau_job_id, cid),
+    FOREIGN KEY (bacalhau_job_id) REFERENCES jobs(bacalhau_job_id),
+    FOREIGN KEY (cid) REFERENCES data_files(cid)
+);
+
+CREATE TABLE job_outputs (
+    bacalhau_job_id VARCHAR(255) NOT NULL,
+    cid VARCHAR(255) NOT NULL,
+    PRIMARY KEY (bacalhau_job_id, cid),
+    FOREIGN KEY (bacalhau_job_id) REFERENCES jobs(bacalhau_job_id),
+    FOREIGN KEY (cid) REFERENCES data_files(cid)
+);
