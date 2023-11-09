8b1a0c4c171d4682d0b878396358df510523460f
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-11-09 10:08:26 -0500

    Update selectors.ts

--- frontend/lib/redux/slices/dataFileListSlice/selectors.ts
+++ frontend/lib/redux/slices/dataFileListSlice/selectors.ts
@@ -1,7 +1,6 @@
 import type { ReduxState } from '@/lib/redux'
 
 export const selectDataFileList = (state: ReduxState) => state.dataFileList.dataFiles
-// export const selectDataFileListLoading = (state: ReduxState) => state.dataFileList.loading
 export const selectDataFileListLoading = (state: ReduxState) => state.dataFileList.status === 'loading';
 export const selectDataFileListSuccess = (state: ReduxState) => state.dataFileList.success
 export const selectDataFileListError = (state: ReduxState) => state.dataFileList.error
