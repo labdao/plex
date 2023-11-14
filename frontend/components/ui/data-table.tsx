373d7bf9061ae9a6137eafbe8bced447ff49af9e
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-11-13 19:25:11 -0500

    modifications to dataTable component to enable sorting functionality

--- frontend/components/ui/data-table.tsx
+++ frontend/components/ui/data-table.tsx
@@ -1,19 +1,28 @@
 "use client";
 
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { ColumnDef, flexRender, getCoreRowModel, getSortedRowModel, SortingState, useReactTable } from "@tanstack/react-table";
+import { useState } from "react";
 
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
+  sorting?: SortingState;
 }
 
-export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+export function DataTable<TData, TValue>({ columns, data, sorting: initialSorting }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>(initialSorting || []);
+
   const table = useReactTable({
     data,
     columns,
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
   });
 
   return (
@@ -23,18 +32,33 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
           <TableRow key={headerGroup.id}>
             {headerGroup.headers.map((header) => {
               return (
-                <TableHead key={header.id}>{header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}</TableHead>
+                <TableHead 
+                  key={header.id}
+                  onClick={() => {
+                    // Custom toggle logic
+                    const isSortedDesc = header.column.getIsSorted() === 'desc';
+                    header.column.toggleSorting(!isSortedDesc); // Toggle between 'asc' and 'desc'
+                  }}
+                >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(header.column.columnDef.header, header.getContext())
+                  }
+                  {header.column.getIsSorted() === 'desc' ? ' 🔽' : header.column.getIsSorted() === 'asc' ? ' 🔼' : ''}
+                </TableHead>
               );
             })}
           </TableRow>
         ))}
       </TableHeader>
       <TableBody>
-        {table.getRowModel().rows?.length ? (
+        {table.getRowModel().rows.length > 0 ? (
           table.getRowModel().rows.map((row) => (
             <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
               {row.getVisibleCells().map((cell) => (
-                <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                <TableCell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
               ))}
             </TableRow>
           ))
@@ -47,5 +71,5 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
         )}
       </TableBody>
     </Table>
-  );
+  );  
 }
