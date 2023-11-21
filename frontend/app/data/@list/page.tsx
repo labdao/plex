d4234936348d05802b39f13b07ea59276bdd6fac
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-11-21 07:40:28 -0600

    tags displayed in table listing datafiles

--- frontend/app/data/@list/page.tsx
+++ frontend/app/data/@list/page.tsx
@@ -8,34 +8,72 @@ import React, { useEffect, useState } from "react";
 import { DataTable } from "@/components/ui/data-table";
 
 export default function ListDataFiles() {
+  interface Tag {
+    Name: string;
+    Type: string;
+  }
+
   interface DataFile {
     CID: string;
     WalletAddress: string;
     Filename: string;
     Timestamp: string;
+    Tags: Tag[];
   }
 
   const shortenAddressOrCid = (addressOrCid: string) => {
-    if (addressOrCid.length) {
-      return `${addressOrCid.substring(0, 6)}...${addressOrCid.substring(addressOrCid.length - 4)}`;
-    } else {
-      return "";
+    if (addressOrCid) {
+      if (addressOrCid.length) {
+        return `${addressOrCid.substring(0, 6)}...${addressOrCid.substring(addressOrCid.length - 4)}`;
+      } else {
+        return "";
+      }
     }
   }
 
   const columns: ColumnDef<DataFile>[] = [
     {
       accessorKey: "Filename",
-      header: "Filename",
+      header: "File",
       enableSorting: true,
       sortingFn: "alphanumeric",
       cell: ({ row }) => {
+        let cid = row.getValue("CID");
+        if (!cid) {
+          cid = "null";
+        }
         return (
-          <a target="_blank" href={`${backendUrl()}/datafiles/${row.getValue("CID")}/download`}>
-            {row.getValue("Filename")}
-          </a>
+          <div>
+            <a target="_blank" href={`${backendUrl()}/datafiles/${row.getValue("CID")}/download`}>
+                {row.getValue("Filename")}
+            </a>
+            <div style={{ fontSize: 'smaller', marginTop: '4px' }}>
+              <a 
+                target="_blank" 
+                href={`${process.env.NEXT_PUBLIC_IPFS_GATEWAY_ENDPOINT}${row.getValue("CID")}/`}
+                style={{ color: 'gray', textDecoration: 'none' }}
+              >
+                {row.getValue("CID")}
+              </a>
+            </div>
+          </div>
+
         );
       },
+    },
+    {
+      accessorKey: "Tags",
+      header: "Tags",
+      cell: ({ row }) => {
+        const tags: Tag[] = row.getValue("Tags") as Tag[];
+        return (
+          <div>
+            {tags.map((tag, index) => (
+              <div key={index}>{tag.Name}</div>
+            ))}
+          </div>
+        )
+      }
     },
     {
       accessorKey: "CID",
@@ -57,6 +95,7 @@ export default function ListDataFiles() {
         return format(new Date(row.getValue("Timestamp")), "yyyy-MM-dd HH:mm:ss")
       }
     },
+
   ];
 
   const [datafiles, setDataFiles] = useState<DataFile[]>([]);
