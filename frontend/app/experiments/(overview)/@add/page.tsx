c65fe9aebf8cccf3db5209af4a947d03c81c8160
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-11-28 12:29:48 -0500

    removes redundant add experiment button

--- frontend/app/experiments/(overview)/@add/page.tsx
+++ frontend/app/experiments/(overview)/@add/page.tsx
@@ -36,7 +36,7 @@ import {
   setFlowAddTool,
   toolListThunk,
 } from "@/lib/redux";
-import { DataFile } from "@/lib/redux/slices/dataFileListSlice/slice";
+import { DataFile } from '@/lib/redux/slices/dataFileListSlice/slice';
 
 export default function AddGraph() {
   const dispatch = useDispatch<AppDispatch>();
@@ -48,6 +48,19 @@ export default function AddGraph() {
   const error = useSelector(selectFlowAddError);
   const kwargs = useSelector(selectFlowAddKwargs);
   const cid = useSelector(selectFlowAddCid);
+
+  interface ToolInput {
+    position?: string;
+    glob?: string[];
+    type: string;
+    default?: string;
+  }
+
+  interface ToolJson {
+    inputs: Record<string, ToolInput>;
+  }
+  
+
   const selectedTool = useSelector(selectFlowAddTool);
   const toolListError = useSelector(selectToolListError);
   const dataFileListError = useSelector(selectDataFileListError);
@@ -57,11 +70,13 @@ export default function AddGraph() {
   const [selectedToolIndex, setSelectedToolIndex] = useState("");
   const [inputDataFiles, setInputDataFiles] = useState<Record<string, DataFile[]>>({});
 
+  
+
   useEffect(() => {
     if (cid !== "") {
       dispatch(setFlowAddSuccess(false));
       dispatch(setFlowAddKwargs({}));
-      dispatch(setFlowAddTool({ CID: "", WalletAddress: "", Name: "", ToolJson: { inputs: {}, name: "", author: "", description: "", github: "", paper: "" }}));
+      dispatch(setFlowAddTool({ CID: "", WalletAddress: "", Name: "", ToolJson: { inputs: {} } }));
       dispatch(setFlowAddError(null));
       dispatch(setFlowAddName(""));
       dispatch(setFlowAddCid(""));
@@ -72,28 +87,51 @@ export default function AddGraph() {
     dispatch(dataFileListThunk());
   }, [cid, dispatch, router]);
 
+  // const handleToolChange = async (value: string) => {
+  //   const selectedTool = tools[parseInt(value)];
+  //   dispatch(setFlowAddTool(selectedTool));
+  //   setSelectedToolIndex(value);
+  
+  //   const newInputDataFiles: Record<string, DataFile[]> = {};
+  
+  //   for (const inputKey in selectedTool.ToolJson.inputs) {
+  //     const input = (selectedTool.ToolJson.inputs as Record<string, { glob: string[] }>)[inputKey];
+  //     if (typeof input === 'object' && input !== null && 'glob' in input) {
+  //       const globPatterns = input.glob;
+  //       const action = await dispatch(dataFileListThunk(globPatterns)) as PayloadAction<DataFile[]>;
+  //       newInputDataFiles[inputKey] = action.payload;
+  //     }
+  //   }
+  
+  //   setInputDataFiles(newInputDataFiles);
+  // };
+
   const handleToolChange = async (value: string) => {
     const selectedTool = tools[parseInt(value)];
     dispatch(setFlowAddTool(selectedTool));
     setSelectedToolIndex(value);
-
+  
     const newInputDataFiles: Record<string, DataFile[]> = {};
-
+    const newKwargs = { ...kwargs };
+  
     for (const inputKey in selectedTool.ToolJson.inputs) {
-      const input = (selectedTool.ToolJson.inputs as Record<string, { glob: string[] }>)[inputKey];
-      if (typeof input === "object" && input !== null && "glob" in input) {
-        const globPatterns = input.glob;
-        const action = (await dispatch(dataFileListThunk(globPatterns))) as PayloadAction<DataFile[]>;
-        newInputDataFiles[inputKey] = action.payload;
+      const input = (selectedTool.ToolJson.inputs as Record<string, { glob: string[]; type: string; default: string }>)[inputKey];
+      if (input.type === 'File') {
+        if (typeof input === 'object' && input !== null && 'glob' in input) {
+          const globPatterns = input.glob;
+          const action = await dispatch(dataFileListThunk(globPatterns)) as PayloadAction<DataFile[]>;
+          newInputDataFiles[inputKey] = action.payload;
+        }
+      } else {
+        newKwargs[inputKey] = [input.default];
       }
     }
-
+  
     setInputDataFiles(newInputDataFiles);
+    dispatch(setFlowAddKwargs(newKwargs));
   };
 
   const handleKwargsChange = (value: string, key: string) => {
-    console.log(value);
-    console.log(key);
     const updatedKwargs = { ...kwargs, [key]: [value] };
     dispatch(setFlowAddKwargs(updatedKwargs));
   };
@@ -125,9 +163,9 @@ export default function AddGraph() {
 
   return (
     <Dialog>
-      <DialogTrigger>
+      {/* <DialogTrigger>
         <Button size="lg">Add Experiment</Button>
-      </DialogTrigger>
+      </DialogTrigger> */}
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Add Experiment</DialogTitle>
@@ -157,16 +195,43 @@ export default function AddGraph() {
                 </Select>
               </div>
 
-              {Object.keys(selectedTool.ToolJson.inputs).map((key) => {
+              {Object.keys(selectedTool.ToolJson.inputs)
+                .sort((a, b) => {
+                  // // @ts-ignore
+                  // const positionA = parseInt(selectedTool.ToolJson.inputs[a].position);
+                  // // @ts-ignore
+                  // const positionB = parseInt(selectedTool.ToolJson.inputs[b].position);
+                  // console.log("some sorting happening....", positionA, positionB)
+                  // return positionA - positionB;
+                  // @ts-ignore
+                  const positionA = parseInt(a[1].position, 10);
+                  // @ts-ignore
+                  const positionB = parseInt(b[1].position, 10);
+                  return positionA - positionB;
+                })
+                .map((key) => {
                 // @ts-ignore
                 const inputDetail = selectedTool.ToolJson.inputs[key];
                 return (
                   <div key={key}>
                     <Label>
                       {key}
-                      {inputDetail.glob && ` (Glob: ${inputDetail.glob.join(", ")})`}
+                      {inputDetail.glob && ` (${inputDetail.glob.join(', ')})`}
+                      {` (${inputDetail.type})`}
                     </Label>
-                    <DataFileSelect onChange={(value) => handleKwargsChange(value, key)} value={kwargs[key]?.[0]} label={key} />
+                  {inputDetail.type === 'File' ? (
+                    <DataFileSelect 
+                      onSelect={(value) => handleKwargsChange(value, key)} 
+                      value={kwargs[key]?.[0]} 
+                      dataFiles={inputDataFiles[key] || []} 
+                      label={key} 
+                    />
+                  ) : (
+                    <Input 
+                      value={kwargs[key]?.[0]} 
+                      onChange={(e) => handleKwargsChange(e.target.value, key)} 
+                    />
+                  )}
                   </div>
                 );
               })}
@@ -182,4 +247,81 @@ export default function AddGraph() {
       </DialogContent>
     </Dialog>
   );
+  // return (
+  //   <Dialog>
+  //     <DialogTrigger>
+  //       <Button size="lg">Add Experiment</Button>
+  //     </DialogTrigger>
+  //     <DialogContent>
+  //       <DialogHeader>
+  //         <DialogTitle>Add Experiment</DialogTitle>
+  //         <DialogDescription>
+  //           <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+  //             <div>
+  //               <Label>Name</Label>
+  //               <Input value={name} onChange={(e) => dispatch(setFlowAddName(e.target.value))} />
+  //             </div>
+  //             <div>
+  //               <Label>Tool</Label>
+  //               <Select onValueChange={handleToolChange} defaultValue={selectedToolIndex}>
+  //                 <SelectTrigger>
+  //                   <SelectValue placeholder="Select a tool" />
+  //                 </SelectTrigger>
+  //                 <SelectContent>
+  //                   <SelectGroup>
+  //                     {tools.map((tool, index) => (
+  //                       <SelectItem key={index} value={index.toString()}>
+  //                         {tool.Name}
+  //                       </SelectItem>
+  //                     ))}
+  //                   </SelectGroup>
+  //                 </SelectContent>
+  //               </Select>
+  //             </div>
+  
+  //             {Object.entries(selectedTool.ToolJson.inputs)
+  //               .sort((a, b) => {
+  //                 // @ts-ignore
+  //                 const positionA = parseInt(a[1].position, 10);
+  //                 // @ts-ignore
+  //                 const positionB = parseInt(b[1].position, 10);
+  //                 console.log("some sorting happening....", positionA, positionB)
+  //                 return positionA - positionB;
+  //               })
+  //               .map(([key, inputDetail]) => (
+  //                 <div key={key}>
+  //                   <Label>
+  //                     {key}
+  //                     {inputDetail.glob && ` (${inputDetail.glob.join(', ')})`}
+  //                     {` (${inputDetail.type})`}
+  //                   </Label>
+  //                   {inputDetail.type === 'File' ? (
+  //                     <DataFileSelect 
+  //                       onSelect={(value) => handleKwargsChange(value, key)} 
+  //                       value={kwargs[key]?.[0]} 
+  //                       dataFiles={inputDataFiles[key] || []} 
+  //                       label={key} 
+  //                     />
+  //                   ) : (
+  //                     <Input 
+  //                       value={kwargs[key]?.[0]} 
+  //                       onChange={(e) => handleKwargsChange(e.target.value, key)} 
+  //                     />
+  //                   )}
+  //                 </div>
+  //               ))}
+  
+  //             <Button type="submit" disabled={loading || !isValidForm()}>
+  //               {loading ? "Submitting..." : "Submit"}
+  //             </Button>
+  //             {error && <Alert variant="destructive">{error}</Alert>}
+  //             {toolListError && <Alert variant="destructive">{toolListError}</Alert>}
+  //             {dataFileListError && <Alert variant="destructive">{dataFileListError}</Alert>}
+  //           </form>
+  //         </DialogDescription>
+  //       </DialogHeader>
+  //     </DialogContent>
+  //   </Dialog>
+  // );
+  
 }
