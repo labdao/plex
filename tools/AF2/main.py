0901344886c81097d807c9a7e0372aec0e5c655d
Ubuntu <ubuntu@ip-172-31-25-109.ec2.internal>
2023-12-07 10:04:06 +0000

    updated parsing function for summary csv

--- tools/AF2/main.py
+++ tools/AF2/main.py
@@ -21,19 +21,42 @@ def parse_log_and_fasta_to_csv(fasta_path, log_path):
             else:
                 sequences[identifier] = line.strip()
 
+    # Initialize the columns for the CSV
+    columns = ['Sequence', 'Query', 'structure_pdbs', 'recycle', 'pLDDT', 'pTM', 'tol', 'rank']
+    
     # Parse the log file and write to CSV
     csv_path = os.path.join(os.path.dirname(log_path), 'summary.csv')
     with open(log_path, 'r') as log_file, open(csv_path, 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(['Sequence', 'Query', 'Data'])
+        csv_writer = csv.DictWriter(csv_file, fieldnames=columns)
+        csv_writer.writeheader()
 
-        current_seq = ''
+        current_query = ''
         for line in log_file:
             if 'Query' in line:
-                current_seq = line.split(':')[1].strip()
-            if 'pLDDT' in line:
-                data = line.split(',')[1].strip()
-                csv_writer.writerow([sequences.get(current_seq, ''), current_seq, data])
+                # Extracting the query value (e.g., "1/2" or "2/2")
+                current_query = line.split('Query')[1].split(':')[0].strip()
+
+            if 'pLDDT' in line and 'pTM' in line:
+                parts = line.split()
+                data = parts[2:]  # Skip the first two substrings (date and time)
+                row_data = {col: '' for col in columns}  # Initialize row with empty values
+
+                # Extract the sequence number (XXX in "XXX/YYY") and get the corresponding sequence
+                sequence_number = current_query.split('/')[0]
+                row_data['Sequence'] = sequences.get(sequence_number, '')
+                row_data['Query'] = current_query
+
+                # Handle the first substring without an equal sign
+                row_data['structure_pdbs'] = data[0] if data else ''
+
+                # Process remaining substrings with equal signs
+                for item in data[1:]:
+                    if '=' in item:
+                        key, value = item.split('=')
+                        if key in columns:
+                            row_data[key] = value
+
+                csv_writer.writerow(row_data)
 
     print(f"CSV file written to {csv_path}")
 
@@ -105,7 +128,7 @@ def my_app(cfg: DictConfig) -> None:
 
     seq2struc(df, outputs_directory, cfg)
 
-    # Example usage
+    # create and write a csv file with sequence and metric information for each output structure
     parse_log_and_fasta_to_csv(os.path.abspath(fasta_file), os.path.join(outputs_directory, 'log.txt'))
 
     print("Sequence to structure complete...")
