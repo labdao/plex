4249ce0ad29d73fafb59cfe6a746095a02eb208d
Aakaash Meduri <aakaash.meduri@gmail.com>
2023-12-21 18:37:47 -0600

    fixed ranges for pagination

--- frontend/components/ui/pagination.tsx
+++ frontend/components/ui/pagination.tsx
@@ -8,50 +8,76 @@ interface PaginationProps {
 
 export const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPageChange }) => {
     const renderPageNumbers = () => {
-      const pageNumbers: (number | string)[] = [];
-      for (let i = 1; i <= Math.min(10, totalPages); i++) {
-        pageNumbers.push(i);
-      }
-  
-      if (totalPages > 10) {
-        if (currentPage > 6) {
-          pageNumbers.splice(1, currentPage - 5, '...');
+        const pageNumbers: (number | string)[] = [];
+
+        if (totalPages <= 10) {
+            for (let i = 1; i <= totalPages; i++) {
+                pageNumbers.push(i);
+            }
+        } else {
+            pageNumbers.push(1);
+
+            let rangeStart = Math.max(2, currentPage - 4);
+            let rangeEnd = Math.min(totalPages - 1, currentPage + 4);
+
+            if (currentPage <= 6) {
+                rangeStart = 2;
+                rangeEnd = 10;
+            }
+
+            if (currentPage >= totalPages - 5) {
+                rangeStart = totalPages - 9;
+                rangeEnd = totalPages - 1;
+            }
+
+            if (rangeStart > 2) {
+                pageNumbers.push('...');
+            }
+
+            for (let i = rangeStart; i <= rangeEnd; i++) {
+                pageNumbers.push(i);
+            }
+
+            if (rangeEnd < totalPages - 1) {
+                pageNumbers.push('...');
+            }
+
+            pageNumbers.push(totalPages);
         }
-        pageNumbers.push(totalPages);
-      }
-  
-      return pageNumbers;
+
+        return pageNumbers;
     };
-  
+
     return (
-      <div className="pagination-controls" style={{ marginTop: '20px', textAlign: 'center' }}>
-        <button 
-            onClick={() => onPageChange(1)} 
-            disabled={currentPage === 1}
-            style={{ marginRight: '10px' }}    
-        >
-            First
-        </button>
-        {renderPageNumbers().map((page, index) => (
-          page === '...' ? 
-            <span key={index} style={{ margin: '0 5px' }}>...</span> : 
+        <div className="pagination-controls" style={{ marginTop: '20px', textAlign: 'center' }}>
             <button 
-              key={page} 
-              onClick={() => onPageChange(typeof page === 'number' ? page : currentPage + 5)} 
-              disabled={currentPage === page}
-              style={{ margin: '0 5px' }}
+                onClick={() => onPageChange(1)} 
+                disabled={currentPage === 1}
+                style={{ marginRight: '10px' }}    
             >
-              {page}
+                First
             </button>
-        ))}
-        <button 
-            onClick={() => onPageChange(totalPages)} 
-            disabled={currentPage === totalPages}
-            style={{ marginLeft: '10px' }}
-        >
-            Last
-        </button>
-      </div>
+            {renderPageNumbers().map((page, index) => (
+                typeof page === 'number' ? (
+                    <button 
+                        key={page} 
+                        onClick={() => onPageChange(page)} 
+                        disabled={currentPage === page}
+                        style={{ margin: '0 5px' }}
+                    >
+                        {page}
+                    </button>
+                ) : (
+                    <span key={index} style={{ margin: '0 5px' }}>...</span>
+                )
+            ))}
+            <button 
+                onClick={() => onPageChange(totalPages)} 
+                disabled={currentPage === totalPages}
+                style={{ marginLeft: '10px' }}
+            >
+                Last
+            </button>
+        </div>
     );
 };
-  
