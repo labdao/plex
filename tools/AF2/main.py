80d5ed580023ca15fa5af6d65ebbeb22b437f6c6
phiflei <https://phiflei@github.com>
2024-01-09 12:57:17 +0100

    cosmetic changes in main

--- tools/AF2/main.py
+++ tools/AF2/main.py
@@ -1,4 +1,3 @@
-# import glob
 import os
 import time
 import pandas as pd
@@ -15,37 +14,6 @@ from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, OmegaConf
 import glob
 import json
-import subprocess
-
-# def prodigy_run(csv_path):
-#     # print('csv path', csv_path)
-#     df = pd.read_csv(csv_path)
-#     # print('csv file data frame', df)
-#     for i, r in df.iterrows():
-
-#         file_path = r['pdb']
-#         if pd.notna(file_path):
-#             print('file path', file_path)
-#             try:
-#                 subprocess.run(
-#                     ["prodigy", "-q", file_path], stdout=open("temp.txt", "w"), check=True
-#                 )
-#                 with open("temp.txt", "r") as f:
-#                     lines = f.readlines()
-#                     if lines:  # Check if lines is not empty
-#                         affinity = float(lines[0].split(" ")[-1].split("/")[0])
-#                         df.loc[i, "affinity"] = affinity
-#                     else:
-#                         # print(f"No output from prodigy for {r['path']}")
-#                         print(f"No output from prodigy for {file_path}")
-#                         # Handle the case where prodigy did not produce output
-#             except subprocess.CalledProcessError:
-#                 # print(f"Prodigy command failed for {r['path']}")
-#                 print(f"Prodigy command failed for {file_path}")
-
-#     # export results
-#     df.to_csv(f"{csv_path}", index=None)
-
 import subprocess
 
 def compute_affinity(file_path):
@@ -103,10 +71,6 @@ def compute_ipae(pdb_file, pae_matrix):
 
     # extract interface values
     interface_pae_values = pae_matrix[contact_indices[:, 0], contact_indices[:, 1]]
-
-    # # Output the results
-    # print("Contact residues (receptor, ligand):", contact_indices)
-    # print("PAE values at the interface:", interface_pae_values)
 
     # Compute the median PAE for the interface contacts
     median_pae_interface = np.median(interface_pae_values)
@@ -171,63 +135,6 @@ def create_summary(directory, json_pattern):
             df.to_csv(summary_file, index=False)
 
     return df
-
-def parse_log_and_fasta_to_csv(fasta_path, log_path): # remove this
-    # Read and store sequences from the FASTA file
-    sequences = {}
-    with open(fasta_path, 'r') as fasta_file:
-        identifier = ''
-        for line in fasta_file:
-            if line.startswith('>'):
-                identifier = line[1:].strip()
-            else:
-                sequences[identifier] = line.strip()
-
-    # Initialize the columns for the CSV
-    columns = ['Sequence', 'Query', 'structure_pdbs', 'recycle', 'pLDDT', 'pTM', 'tol', 'rank']
-    
-    # Parse the log file and write to CSV
-    csv_path = os.path.join(os.path.dirname(log_path), 'summary.csv')
-    with open(log_path, 'r') as log_file, open(csv_path, 'w', newline='') as csv_file:
-        csv_writer = csv.DictWriter(csv_file, fieldnames=columns)
-        csv_writer.writeheader()
-
-        current_query = ''
-        for line in log_file:
-            if 'Query' in line:
-                # Extracting the query value (e.g., "1/2" or "2/2")
-                current_query = line.split('Query')[1].split(':')[0].strip()
-
-            if 'pLDDT' in line and 'pTM' in line:
-                parts = line.split()
-                data = parts[2:]  # Skip the first two substrings (date and time)
-                row_data = {col: '' for col in columns}  # Initialize row with empty values
-
-                # Extract the sequence number (XXX in "XXX/YYY") and get the corresponding sequence
-                sequence_number = current_query.split('/')[0]
-                row_data['Sequence'] = sequences.get(sequence_number, '')
-                row_data['Query'] = current_query
-
-                # Handle the first substring without an equal sign
-                row_data['structure_pdbs'] = data[0] if data else ''
-
-                # Process remaining substrings with equal signs
-                for item in data[1:]:
-                    if '=' in item:
-                        key, value = item.split('=')
-                        if key in columns:
-                            row_data[key] = value
-
-                csv_writer.writerow(row_data)
-
-
-    
-    # print("running Prodigy")
-    # prodigy_run(f"{self.outputs_directory}/folding_with_target_summary.csv")
-
-    # need to write the prodigy scores to the csv; for this initialize the columns above
-
-    print(f"CSV file written to {csv_path}")
 
 def write_dataframe_to_fastas(dataframe, cfg):
     input_dir = os.path.join(cfg.inputs.directory, 'current_sequences')
@@ -298,7 +205,6 @@ def my_app(cfg: DictConfig) -> None:
     seq2struc(df, outputs_directory, cfg)
 
     # create and write a csv file with sequence and metric information for each output structure
-    # parse_log_and_fasta_to_csv(os.path.abspath(fasta_file), os.path.join(outputs_directory, 'log.txt'))
     create_summary(outputs_directory, json_pattern='seq_1')
 
     print("Sequence to structure complete...")
@@ -308,50 +214,3 @@ def my_app(cfg: DictConfig) -> None:
 
 if __name__ == "__main__":
     my_app()
-
-
-
-# def update_complex_summary(t, row, sequence, sequence_pseudoLL, df, directory, json_pattern):
-#     summary_file = os.path.join(directory, 'folding_with_target_summary.csv')
-
-#     # loop over JSON files that match the given pattern for the current iteration
-#     for json_file in glob.glob(os.path.join(directory, f"{json_pattern}_scores*.json")):
-#         with open(json_file, 'r') as file:
-#             data = json.load(file)
-
-#         # Compute average plddt
-#         avg_plddt = sum(data['plddt']) / len(data['plddt'])
-
-#         # Get max_pae value
-#         max_pae = data['max_pae']
-
-#         # Find corresponding PDB file
-#         pdb_file = None
-#         rank_str = json_file.split('rank')[1].split('.')[0]
-#         for pdb in glob.glob(os.path.join(directory, f"{json_pattern}_unrelaxed_rank{rank_str}*.pdb")):
-#             pdb_file = pdb
-#             break
-
-#         # Prepare new row
-#         new_row = {
-#             't': t, # delete
-#             'target_seq': row['target_seq'].iloc[0],
-#             'binder_seq': row['binder_seq'].iloc[0],
-#             'complex': sequence,
-#             'sequence_pseudo_LL': sequence_pseudoLL, # delete
-#             'mean plddt': avg_plddt,
-#             'max pae': max_pae, # add i_pae # add rmsd
-#             'json': os.path.abspath(json_file),
-#             'pdb': os.path.abspath(pdb_file) if pdb_file else None
-#         }
-
-#         # Concatenate new row to DataFrame
-#         df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
-
-#     if not df.empty:
-#         if os.path.exists(summary_file):
-#             df.to_csv(summary_file, mode='a', header=False, index=False)
-#         else:
-#             df.to_csv(summary_file, index=False)
-
-#     return df
