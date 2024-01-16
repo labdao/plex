07ddf577ce1275fd2d65f789048b55563b6f9f55
Jeff Merrick <jeff@wireform.io>
2024-01-16 17:12:56 -0600

    [LAB-1027] Expected outputs (#837)
    Co-authored-by: Aakaash Meduri <aakaash.meduri@gmail.com>

--- frontend/app/tasks/[slug]/TaskPageHeader.tsx
+++ frontend/app/tasks/[slug]/TaskPageHeader.tsx
@@ -1,45 +1,48 @@
 "use client";
 
-import { BookOpenIcon, GithubIcon } from "lucide-react";
+import { BookOpenIcon, GithubIcon, Loader2Icon } from "lucide-react";
 import React from "react";
 
 import { badgeVariants } from "@/components/ui/badge";
-import { Separator } from "@/components/ui/separator";
 import { ToolDetail } from "@/lib/redux";
 
 interface TaskPageHeaderProps {
   tool: ToolDetail;
   //@TODO will have a proper type once tasks are set up
   task: { name: string; slug: string; default_tool: { CID: string } };
+  loading: boolean;
 }
 
-export default function TaskPageHeader({ tool, task }: TaskPageHeaderProps) {
+export default function TaskPageHeader({ tool, task, loading }: TaskPageHeaderProps) {
   const { author, name, description, github, paper } = tool.ToolJson;
 
   return (
-    <>
+    <div className="mb-6 border-b min-h-[11rem] border-b-border">
       <h1 className="mb-4 text-3xl font-heading">
         <span className="text-muted-foreground">
-          {task.name}/{author || "unknown"}/
+          {task.name}/{loading ? <Loader2Icon className="inline-block opacity-50 animate-spin" /> : <>{author || "unknown"}/</>}
         </span>
-        {name}
+        {!loading && name}
       </h1>
-      <p className="max-w-prose line-clamp-2 min-h-[3em]">{description}</p>
-      {(github || paper) && (
-        <div className="flex gap-2 mt-4">
-          {github && (
-            <a href={github} target="_blank" className={badgeVariants({ variant: "references", size: "lg" })}>
-              github <GithubIcon className="ml-2" size={20} />
-            </a>
+      {!loading && (
+        <>
+          <p className="max-w-prose line-clamp-2 ">{description}</p>
+          {(github || paper) && (
+            <div className="flex gap-2 mt-4 ">
+              {github && (
+                <a href={github} target="_blank" className={badgeVariants({ variant: "references", size: "lg" })}>
+                  github <GithubIcon className="ml-2" size={20} />
+                </a>
+              )}
+              {paper && (
+                <a href={paper} target="_blank" className={badgeVariants({ variant: "references", size: "lg" })}>
+                  paper <BookOpenIcon className="ml-2" size={20} />
+                </a>
+              )}
+            </div>
           )}
-          {paper && (
-            <a href={paper} target="_blank" className={badgeVariants({ variant: "references", size: "lg" })}>
-              paper <BookOpenIcon className="ml-2" size={20} />
-            </a>
-          )}
-        </div>
+        </>
       )}
-      <Separator className="my-6" />
-    </>
+    </div>
   );
 }
--- frontend/app/tasks/[slug]/page.tsx
+++ frontend/app/tasks/[slug]/page.tsx
@@ -10,7 +10,6 @@ import { useDispatch, useSelector } from "react-redux";
 import * as z from "zod";
 
 import ProtectedComponent from "@/components/auth/ProtectedComponent";
-import { PageLoader } from "@/components/shared/PageLoader";
 import { ToolSelect } from "@/components/shared/ToolSelect";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -24,7 +23,7 @@ import { createFlow } from "@/lib/redux/slices/flowAddSlice/asyncActions";
 import { DynamicArrayField } from "./DynamicArrayField";
 import { generateDefaultValues, generateSchema } from "./formGenerator";
 import TaskPageHeader from "./TaskPageHeader";
-import { VariantSummary } from "./VariantSummary";
+import { TaskSummary } from "./TaskSummary";
 
 type JsonValueArray = Array<{ value: any }>; // Define the type for the array of value objects
 
@@ -172,7 +171,6 @@ export default function TaskDetail({ params }: { params: { slug: string } }) {
       // Handle error, maybe show message to user
     }
   }
-
   return (
     <>
       <div className="container mt-8">
@@ -183,9 +181,9 @@ export default function TaskDetail({ params }: { params: { slug: string } }) {
           </Alert>
         )}
         <>
-          {!toolDetailLoading && <TaskPageHeader tool={tool} task={task} />}
+          <TaskPageHeader tool={tool} task={task} loading={toolDetailLoading} />
           <ProtectedComponent method="overlay" message="Log in to run an experiment">
-            <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+            <div className="grid min-h-screen grid-cols-1 gap-8 lg:grid-cols-3">
               <div className="col-span-2">
                 <Form {...form}>
                   <form id="task-form" onSubmit={form.handleSubmit((values) => onSubmit(values))} className="space-y-8">
@@ -278,7 +276,7 @@ export default function TaskDetail({ params }: { params: { slug: string } }) {
                 </Form>
               </div>
               <div>
-                <VariantSummary sortedInputs={sortedInputs} form={form} />
+                <TaskSummary sortedInputs={sortedInputs} form={form} outputs={tool?.ToolJson?.outputs} />
               </div>
             </div>
           </ProtectedComponent>
