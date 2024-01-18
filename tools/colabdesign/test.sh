645291aea3c1fb8a68ea906ba9826b0aa9447007
The Technocrat <josh.mcmenemy@openzyme.bio>
2024-01-18 06:48:42 -0800

    Update test.sh

--- tools/colabdesign/test.sh
+++ tools/colabdesign/test.sh
@@ -9,13 +9,6 @@ mkdir -p "$PWD/$OUTPUT_DIR"
 echo "Output directory is $OUTPUT_DIR"
 
 # Run the Docker container
-# docker run --gpus=all -v $PWD/testdata/pdc_upar_1_target.pdb:/inputs/target_protein/pdc_upar_1_target.pdb \
-# -v $PWD/testdata/VTNCparams1.yaml:/app/conf/params/VTNCparams1.yaml \
-# -v "$PWD/$OUTPUT_DIR":/outputs colabdesign-noninteractive python -u main.py inputs=container outputs=container params=VTNCparams1
-
-# This is an advanced overide example that can be used instead
-# -e PLEX_JOB_INPUTS='{"binder_length":50,"hotspot":"","number_of_binders":2,"target_chain":"B","target_start_residue":50,"target_end_residue":200,"target_protein":"/inputs/target_protein/pdc_upar_1_target.pdb","contigs_override":"A1-283:11/2/5/11/11"}' \
-
 docker run --gpus=all \
 -e PLEX_JOB_INPUTS='{"binder_length":80,"hotspot":"B58,B80,B139","number_of_binders":2,"target_chain":"B","target_start_residue":17,"target_end_residue":209,"target_protein":"/inputs/target_protein/3di3.pdb","contigs_override":""}' \
 -v $PWD/testdata/inputs:/inputs/ \
