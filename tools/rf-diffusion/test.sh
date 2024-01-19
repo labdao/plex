d9be0e790e8f92c58bf4d67900fcb491112ee276
phiflei <146727682+phiflei@users.noreply.github.com>
2024-01-19 10:35:17 +0100

    Create test.sh

---
+++ tools/rf-diffusion/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build the Docker image
+docker build -t pf/sequence-sampler-updated .
+
+# Run the Docker container
+docker run -it --gpus all \
+  -v /home/convexity-research/philipp/lab-exchange/tools/sequence-sampler/:/inputs \
+  -v /home/convexity-research/philipp/lab-exchange/tools/sequence-sampler/outputs/:/app/outputs \
+  pf/sequence-sampler-updated:latest
