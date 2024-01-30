f6ea13c7a68560ed427e91248547d591280819e8
Aakaash Meduri <aakaash.meduri@gmail.com>
2024-01-30 18:26:48 -0500

    models and migrations aligned with queue

--- gateway/models/flow.go
+++ gateway/models/flow.go
@@ -3,7 +3,8 @@ package models
 import "time"
 
 type Flow struct {
-	CID           string    `gorm:"primaryKey;column:cid;type:varchar(255);not null"` // column name specified to avoid GORM default snake case
+	ID            uint      `gorm:"primaryKey;autoIncrement"`
+	CID           string    `gorm:"column:cid;type:varchar(255);not null"`
 	Jobs          []Job     `gorm:"foreignKey:FlowID"`
 	Name          string    `gorm:"type:varchar(255);"`
 	WalletAddress string    `gorm:"type:varchar(42);not null"`
