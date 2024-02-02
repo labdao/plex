86aebd9f71f3076103a9cfb253bd5968215d126f
Aakaash Meduri <aakaash.meduri@gmail.com>
2024-02-02 06:36:59 -0500

    api key table migrations

---
+++ gateway/migrations/6_add_api_keys_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE api_keys (
+    id SERIAL PRIMARY KEY,
+    key VARCHAR(255) NOT NULL UNIQUE,
+    scope VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    user_id VARCHAR(42) NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(wallet_address)
+);
