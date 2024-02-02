7689bb9e4978c683ce8267904af4a313afaa048f
Aakaash Meduri <aakaash.meduri@gmail.com>
2024-02-02 06:36:24 -0500

    user struct has api keys associated

--- gateway/models/user.go
+++ gateway/models/user.go
@@ -5,4 +5,5 @@ import "time"
 type User struct {
 	WalletAddress string    `gorm:"primaryKey;type:varchar(42);not null" json:"walletAddress"`
 	CreatedAt     time.Time `gorm:""`
+	APIKeys       []APIKey  `gorm:"foreignKey:UserID"`
 }
