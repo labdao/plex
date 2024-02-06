bca37bcc653766527deb492176efc0f9c1d57a8e
Jeff Merrick <jeff@wireform.io>
2024-02-06 14:40:01 -0600

    Smaller panel size

--- frontend/app/experiments/layout.tsx
+++ frontend/app/experiments/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children, panel }: { children: React.ReactNode; panel: React.ReactNode }) {
   return (
-    <div className="grid grid-flow-col auto-cols-fr">
+    <div className="grid grid-flow-col auto-cols-fr lg:auto-cols-auto lg:grid-cols-[minmax(auto,_1fr)]">
       {children}
       {panel}
     </div>
