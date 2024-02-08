a3d7bdbb6e78e33787bb7a01551e5b7beb5c59fd
Aakaash Meduri <aakaash.meduri@gmail.com>
2024-02-08 12:30:31 -0500

    removes commented out handler

--- gateway/handlers/api.go
+++ gateway/handlers/api.go
@@ -1,33 +1,105 @@
 package handlers
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 	"time"
 
+	"github.com/labdao/plex/gateway/middleware"
 	"github.com/labdao/plex/gateway/models"
 	"github.com/labdao/plex/gateway/utils"
 
 	"gorm.io/gorm"
 )
 
-func GenerateAPIKeyHandler(db *gorm.DB) http.HandlerFunc {
+func AddAPIKeyHandler(db *gorm.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		apiKeyString, err := utils.GenerateAPIKey(32, "test")
-		if err != nil {
-			http.Error(w, "Error generating API key", http.StatusInternalServerError)
+		if r.Method != http.MethodPost {
+			utils.SendJSONError(w, "Only POST method is supported", http.StatusBadRequest)
 			return
 		}
 
-		createdAt := time.Now()
-		expiresAt := createdAt.Add(30 * 24 * time.Hour)
+		token, err := utils.ExtractAuthHeader(r)
+		if err != nil {
+			utils.SendJSONError(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
 
-		scope := models.ScopeReadWrite
+		var walletAddress string
+		if middleware.IsJWT(token) {
+			claims, err := middleware.ValidateJWT(token, db)
+			if err != nil {
+				utils.SendJSONError(w, "Invalid JWT", http.StatusUnauthorized)
+				return
+			}
+			walletAddress, err = middleware.GetWalletAddressFromJWTClaims(claims, db)
+			if err != nil {
+				utils.SendJSONError(w, err.Error(), http.StatusUnauthorized)
+				return
+			}
+		} else {
+			walletAddress, err = middleware.GetWalletAddressFromAPIKey(token, db)
+			if err != nil {
+				utils.SendJSONError(w, err.Error(), http.StatusUnauthorized)
+				return
+			}
+		}
 
-		fmt.Printf("Generated API key: %s, Scope: %s, CreatedAt: %s, ExpiresAt: %s\n", apiKeyString, scope, createdAt.Format(time.RFC3339), expiresAt.Format(time.RFC3339))
+		apiKeyString, err := utils.GenerateAPIKey(32, walletAddress)
+		if err != nil {
+			utils.SendJSONError(w, "Error generating API key", http.StatusInternalServerError)
+			return
+		}
+
+		apiKey := models.APIKey{
+			Key:       apiKeyString,
+			Scope:     models.ScopeReadWrite, // default scope is read-write
+			CreatedAt: time.Now(),
+			ExpiresAt: time.Now().Add(30 * 24 * time.Hour), // default expiration time is 30 days
+			UserID:    walletAddress,
+		}
+
+		result := db.Create(&apiKey)
+		if result.Error != nil {
+			utils.SendJSONError(w, "Error saving API key to database", http.StatusInternalServerError)
+			return
+		}
 
 		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(fmt.Sprintf(`{"apiKey": "%s", "scope": "%s", "createdAt": "%s", "expiresAt": "%s"}`, apiKeyString, scope, createdAt.Format(time.RFC3339), expiresAt.Format(time.RFC3339))))
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(apiKey)
+	}
+}
+
+func ListAPIKeysHandler(db *gorm.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			utils.SendJSONError(w, "Only GET method is supported", http.StatusBadRequest)
+			return
+		}
+
+		did, err := middleware.GetUserDIDFromRequest(r, db)
+		if err != nil {
+			utils.SendJSONError(w, "Failed to get user DID from request: "+err.Error(), http.StatusUnauthorized)
+			return
+		}
+
+		user, err := middleware.GetUserByDID(did, db)
+		if err != nil {
+			utils.SendJSONError(w, "Failed to get user by DID: "+err.Error(), http.StatusNotFound)
+			return
+		}
+
+		var apiKeys []models.APIKey
+		if err := db.Where("user_id = ?", user.WalletAddress).Find(&apiKeys).Error; err != nil {
+			utils.SendJSONError(w, "Failed to get API keys: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(apiKeys); err != nil {
+			utils.SendJSONError(w, "Error encoding API keys to JSON: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 }
