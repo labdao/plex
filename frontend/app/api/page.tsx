856f5d379b90ec7992d77a8f8f475947150c6c76
Aakaash Meduri <aakaash.meduri@gmail.com>
2024-02-09 09:01:55 -0500

    temporary api key page for testing

--- frontend/app/api/page.tsx
+++ frontend/app/api/page.tsx
@@ -1,23 +1,21 @@
-// // frontend/app/api/@page.tsx
-// "use client";
+"use client";
 
-// import { usePrivy } from "@privy-io/react-auth";
-// import { ColumnDef } from "@tanstack/react-table";
+import { usePrivy } from "@privy-io/react-auth";
+import { ColumnDef } from "@tanstack/react-table";
 // import { format } from "date-fns";
-// import React, { useEffect, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-// import { DataTable } from "@/components/ui/data-table";
-// import { Pagination } from "@/components/ui/pagination";
-// import { Button } from "@/components/ui/button";
-// import { AppDispatch, apiKeyListThunk, selectApiKeyList, ApiKey, addApiKeyThunk } from "@/lib/redux";
+import { DataTable } from "@/components/ui/data-table";
+import { Pagination } from "@/components/ui/pagination";
+import { Button } from "@/components/ui/button";
+import { AppDispatch, apiKeyListThunk, selectApiKeyList, ApiKey, addApiKeyThunk } from "@/lib/redux";
 
-// export default function ApiPage() {
-//   const dispatch = useDispatch<AppDispatch>();
-//   const apiKeys = useSelector(selectApiKeyList);
-//   const { getAccessToken } = usePrivy();
+export default function ApiPage() {
+  const dispatch = useDispatch<AppDispatch>();
+  const apiKeys = useSelector(selectApiKeyList);
+  const { getAccessToken } = usePrivy();
 
-//   // Define your columns based on the ApiKey interface
 //   const columns: ColumnDef<ApiKey>[] = [
 //     {
 //         accessorKey: 'key',
@@ -41,29 +39,18 @@
 //       },
 //   ];
 
+  useEffect(() => {
+    dispatch(apiKeyListThunk());
+  }, [dispatch]);
 
-//   useEffect(() => {
-//     dispatch(apiKeyListThunk());
-//   }, [dispatch]);
+  const handleAddApiKey = async () => {
+    const apiKeyPayload = { name: 'New API Key' };
+    dispatch(addApiKeyThunk(apiKeyPayload));
+  }
 
-//   const handleGenerateApiKey = async () => {
-//     // Dispatch the thunk to generate a new API key
-//     dispatch(addApiKeyThunk());
-//   };
-
-//   return (
-//     <div>
-//       <div className="border rounded-lg overflow-hidden">
-//         <DataTable columns={columns} data={apiKeys} sorting={sorting} />
-//       </div>
-//       <div className="my-4">
-//         <Button onClick={handleGenerateApiKey}>Add API Key</Button>
-//       </div>
-//       <Pagination
-//         currentPage={currentPage}
-//         totalPages={totalPages}
-//         onPageChange={(page) => setCurrentPage(page)}
-//       />
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <button onClick={handleAddApiKey}>Add API Key</button>
+    </div>
+  );
+}
