e6f48e8b717816204472d8a8a658bc59fc11d196
thetechnocrat-dev <josh.mcmenemy@openzyme.bio>
2024-02-23 19:27:46 +0000

    switch datafile to uuid

--- gateway/models/datafile.go
+++ gateway/models/datafile.go
@@ -5,11 +5,12 @@ import (
 )
 
 type DataFile struct {
-	CID           string    `gorm:"primaryKey;column:cid;type:varchar(255);not null"`
+	ID            string    `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+	CID           string    `gorm:"index;column:cid;type:varchar(255);not null"`
 	WalletAddress string    `gorm:"type:varchar(42);not null"`
 	Filename      string    `gorm:"type:varchar(255);not null"`
-	InputFiles    []Job     `gorm:"many2many:job_input_files;foreignKey:CID;joinForeignKey:data_file_c_id;inverseJoinForeignKey:job_id"`
-	OutputFiles   []Job     `gorm:"many2many:job_output_files;foreignKey:CID;joinForeignKey:data_file_c_id;inverseJoinForeignKey:job_id"`
-	Tags          []Tag     `gorm:"many2many:datafile_tags;foreignKey:CID;joinForeignKey:data_file_c_id;inverseJoinForeignKey:tag_name"`
 	Timestamp     time.Time `gorm:""`
+	InputFiles    []Job     `gorm:"many2many:job_input_files;foreignKey:ID;joinForeignKey:data_file_id;inverseJoinForeignKey:job_id"`
+	OutputFiles   []Job     `gorm:"many2many:job_output_files;foreignKey:ID;joinForeignKey:data_file_id;inverseJoinForeignKey:job_id"`
+	Tags          []Tag     `gorm:"many2many:datafile_tags;foreignKey:ID;joinForeignKey:data_file_id;inverseJoinForeignKey:tag_name"`
 }
