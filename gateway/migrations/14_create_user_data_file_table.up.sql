a6c966836e2c3055bc4dfb0272d5ee8b0772cfd9
supraja-968 <supraja968@gmail.com>
2024-02-28 17:51:03 +0100

    user_datafile creation script

---
+++ gateway/migrations/14_create_user_data_file_table.up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE user_datafiles (
+    wallet_address varchar(42) NOT NULL,
+    c_id varchar(255) NOT NULL,
+    created_at timestamp,
+    CONSTRAINT pk_user_datafiles PRIMARY KEY (wallet_address, c_id),
+    CONSTRAINT fk_user_datafiles_wallet_address FOREIGN KEY (wallet_address) REFERENCES users(wallet_address),
+    CONSTRAINT fk_user_datafiles_data_file FOREIGN KEY (c_id) REFERENCES data_files(cid)
+);
+
+INSERT INTO user_datafiles (wallet_address, c_id, created_at)
+SELECT wallet_address, cid, COALESCE(timestamp, CURRENT_TIMESTAMP)
+FROM data_files;
+
+-- ALTER TABLE data_files
+-- DROP COLUMN wallet_address,
+-- DROP COLUMN timestamp;
