4ca9f230e74f3b5283324b37a7b5ef4c87f68951
Jeff Merrick <jeff@wireform.io>
2024-03-06 16:51:02 -0600

    Refactor and add enhancements to checkpoint visualization

---
+++ frontend/components/visualization/Molstar/index.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import "./skin.scss";
+
+import { PluginCommands } from "molstar/lib/mol-plugin/commands";
+import { PluginConfig } from "molstar/lib/mol-plugin/config";
+import { createPluginUI } from "molstar/lib/mol-plugin-ui";
+import { PluginUIContext } from "molstar/lib/mol-plugin-ui/context";
+import { renderReact18 } from "molstar/lib/mol-plugin-ui/react18";
+import { DefaultPluginUISpec, PluginUISpec } from "molstar/lib/mol-plugin-ui/spec";
+import { ColorNames } from "molstar/lib/mol-util/color/names";
+import { createRef, useEffect } from "react";
+
+declare global {
+  interface Window {
+    molstar?: PluginUIContext;
+  }
+}
+
+interface MolstarProps {
+  url: string;
+  showControls?: boolean;
+  isExpanded?: boolean;
+  className?: string;
+}
+
+const MolstarViz = ({ url, showControls, isExpanded, className }: MolstarProps) => {
+  const parent = createRef<HTMLDivElement>();
+
+  // In debug mode of react's strict mode, this code will
+  // be called twice in a row, which might result in unexpected behavior.
+  useEffect(() => {
+    const MySpec: PluginUISpec = {
+      ...DefaultPluginUISpec(),
+
+      // See config options here: https://github.com/molstar/molstar/blob/master/src/mol-plugin/config.ts#L25
+      config: [[PluginConfig.Viewport.ShowExpand, false]],
+
+      // See layout options here: https://github.com/molstar/molstar/blob/master/src/mol-plugin/layout.ts#L23
+      layout: {
+        initial: {
+          isExpanded: isExpanded,
+          showControls: showControls,
+        },
+      },
+    };
+    async function init() {
+      try {
+        window.molstar = await createPluginUI({
+          target: parent.current as HTMLDivElement,
+          spec: MySpec,
+          render: renderReact18,
+        });
+
+        const renderer = window.molstar.canvas3d!.props.renderer;
+        PluginCommands.Canvas3D.SetSettings(window.molstar, {
+          settings: { renderer: { ...renderer, backgroundColor: ColorNames.white } },
+        });
+
+        const data = await window.molstar.builders.data.download({ url: url }, { state: { isGhost: true } });
+        const trajectory = await window.molstar.builders.structure.parseTrajectory(data, "pdb");
+        await window.molstar.builders.structure.hierarchy.applyPreset(trajectory, "default");
+      } catch (error) {
+        console.error("Error initializing Molstar:", error);
+      }
+    }
+    init();
+    return () => {
+      window.molstar?.dispose();
+      window.molstar = undefined;
+    };
+  }, [isExpanded, parent, showControls, url]);
+
+  return <div ref={parent} style={{ position: "relative" }} className={className || ""} />;
+};
+
+export default MolstarViz;
