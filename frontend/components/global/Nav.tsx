328bd893d5ddd0c15e79dcd639f3204880bf0708
Jeff Merrick <jeff@wireform.io>
2024-03-12 10:58:05 -0500

    LAB-1120 add powered by convexity to nav

--- frontend/components/global/Nav.tsx
+++ frontend/components/global/Nav.tsx
@@ -11,7 +11,6 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { ExperimentStatus } from "@/app/experiments/ExperimentStatus";
 import { NavButton } from "@/components/global/NavItem";
-import { Button } from "@/components/ui/Button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { AppDispatch, Flow, flowListThunk, selectCategorizedFlows, selectFlowList, selectFlowListLoading } from "@/lib/redux";
 
@@ -85,6 +84,18 @@ export default function Nav() {
         {/* <NavContent>
           <NavLink href="https://github.com/labdao" target="_blank" icon={<GithubIcon />} title="GitHub" />
         </NavContent> */}
+        <NavContent>
+          <div className="flex items-center gap-3 px-3 py-1 text-xs text-muted-foreground/50 ">
+            <svg xmlns="http://www.w3.org/2000/svg" width={15} height={16} fill="none">
+              <path
+                fill="currentColor"
+                d="M0 8c0-2.8 1.1-4.7 3.3-5.6a12 12 0 0 0-1 5.6c0 2.4.3 4.2 1 5.5C1.1 12.5 0 10.7 0 8Zm7.3 6.3c1.6 0 2.9-.2 4-.7-.8 1.6-2.1 2.4-4 2.4s-3.2-.8-4-2.5c1 .6 2.4.8 4 .8ZM7.3 0c2 0 3.1.8 3.8 2.3-1-.4-2.3-.6-3.8-.6s-3 .2-4 .7a4.2 4.2 0 0 1 4-2.4ZM14 5.2h-2.2a9.1 9.1 0 0 0-.7-2.9c1.5.6 2.5 1.6 3 2.9Z"
+              />
+              <path fill="currentColor" d="M12 10.1h2.2a5 5 0 0 1-3 3.5c.4-1 .7-2 .7-3.5Z" />
+            </svg>
+            Powered by <br /> Convexity Labs
+          </div>
+        </NavContent>
         <NavContent>
           <div className="p-2 font-mono text-xs font-bold text-muted-foreground opacity-70">Personal</div>
           <NavLink href="/experiments" icon={<FlaskRoundIcon />} title="Experiments" />
