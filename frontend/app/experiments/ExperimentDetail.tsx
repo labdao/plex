61898cb9893dad77d69bfcccd885108a815df1b2
Aakaash Meduri <aakaash.meduri@gmail.com>
2024-03-15 02:40:06 -0400

    public experiment links hardcoded

--- frontend/app/experiments/ExperimentDetail.tsx
+++ frontend/app/experiments/ExperimentDetail.tsx
@@ -2,6 +2,7 @@
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { Share2 } from "lucide-react";
 import Link from "next/link";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
