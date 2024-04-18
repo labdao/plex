8cd46f87198be343ef8725c76ffba42b86a7527a
Jeff Merrick <jeff@wireform.io>
2024-04-17 23:23:45 -0500

    Updated Model Info Panel (#949)

--- frontend/app/experiments/(experiment)/ExperimentUIContext.tsx
+++ frontend/app/experiments/(experiment)/ExperimentUIContext.tsx
@@ -1,26 +1,31 @@
 import { createContext, useState } from "react";
 
-interface ActiveResultContextType {
+interface ExperimentUIContextType {
   activeJobUUID: string | undefined;
   setActiveJobUUID: React.Dispatch<React.SetStateAction<string | undefined>>;
   activeCheckpointUrl: string | undefined;
   setActiveCheckpointUrl: React.Dispatch<React.SetStateAction<string | undefined>>;
+  modelPanelOpen: boolean;
+  setModelPanelOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
-export const ActiveResultContext = createContext<ActiveResultContextType>({} as ActiveResultContextType);
-export function ActiveResultContextProvider({ children }: { children: React.ReactNode }) {
+export const ExperimentUIContext = createContext<ExperimentUIContextType>({} as ExperimentUIContextType);
+export function ExperimentUIContextProvider({ children }: { children: React.ReactNode }) {
   const [activeJobUUID, setActiveJobUUID] = useState<string | undefined>(undefined);
   const [activeCheckpointUrl, setActiveCheckpointUrl] = useState<string | undefined>(undefined);
+  const [modelPanelOpen, setModelPanelOpen] = useState<boolean>(false);
 
   return (
-    <ActiveResultContext.Provider
+    <ExperimentUIContext.Provider
       value={{
         activeJobUUID,
         setActiveJobUUID,
         activeCheckpointUrl,
         setActiveCheckpointUrl,
+        modelPanelOpen,
+        setModelPanelOpen,
       }}
     >
       {children}
-    </ActiveResultContext.Provider>
+    </ExperimentUIContext.Provider>
   );
 }
