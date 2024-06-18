e3b2b2759b74c190069bbea416e471eaa6399f86
Aakaash Meduri <aakaash.meduri@gmail.com>
2024-06-18 05:33:51 -0400

    select user tier

--- frontend/lib/redux/slices/userSlice/selectors.ts
+++ frontend/lib/redux/slices/userSlice/selectors.ts
@@ -3,4 +3,5 @@ import type { ReduxState } from "@/lib/redux";
 export const selectUserError = (state: ReduxState) => state.user.error;
 export const selectUserWalletAddress = (state: ReduxState) => state.user.walletAddress;
 export const selectUserDID = (state: ReduxState) => state.user.did;
+export const selectUserTier = (state: ReduxState) => state.user.tier;
 export const selectUserIsAdmin = (state: ReduxState) => state.user.isAdmin;
