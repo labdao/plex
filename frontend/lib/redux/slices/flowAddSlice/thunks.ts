906aff973533f95a78ec2bf5472af8a4964d8a5f
Aakaash Meduri <aakaash.meduri@gmail.com>
2024-06-18 09:02:22 -0400

    thunk now passes in flow payload when pinging stripe

--- frontend/lib/redux/slices/flowAddSlice/thunks.ts
+++ frontend/lib/redux/slices/flowAddSlice/thunks.ts
@@ -1,7 +1,9 @@
+import { setStripeCheckoutError, setStripeCheckoutLoading, setStripeCheckoutSuccess, setStripeCheckoutUrl } from '@/lib/redux'
 import { createAppAsyncThunk } from '@/lib/redux/createAppAsyncThunk'
 
+import { getCheckoutURL } from '../stripeCheckoutSlice/asyncActions'
 import { createFlow } from './asyncActions'
-import { Kwargs, setFlowAddError, setFlowAddID,setFlowAddSuccess } from './slice'
+import { Kwargs, setFlowAddError, setFlowAddID, setFlowAddLoading, setFlowAddSuccess } from './slice'
 
 interface FlowPayload {
   name: string,
@@ -34,3 +36,38 @@ export const addFlowThunk = createAppAsyncThunk(
     }
   }
 )
+
+export const addFlowWithCheckoutThunk = createAppAsyncThunk(
+  'flow/addFlowWithCheckout',
+  async ({ toolCid, scatteringMethod, kwargs }: FlowPayload, { dispatch }) => {
+    try {
+      dispatch(setFlowAddError(null));
+      dispatch(setStripeCheckoutError(null));
+      dispatch(setStripeCheckoutLoading(true));
+
+      const checkoutPayload = {
+        toolCid,
+        scatteringMethod,
+        kwargs: JSON.stringify(kwargs),
+      };
+      const checkoutResponse = await getCheckoutURL(checkoutPayload);
+      dispatch(setStripeCheckoutSuccess(true));
+      dispatch(setStripeCheckoutUrl(checkoutResponse));
+      dispatch(setStripeCheckoutLoading(false));
+
+      return { checkout: checkoutResponse };
+    } catch (error: unknown) {
+      console.log('Failed to add flow with checkout.', error);
+      if (error instanceof Error) {
+        dispatch(setFlowAddError(error.message));
+        dispatch(setStripeCheckoutError(error.message));
+      } else {
+        dispatch(setFlowAddError('Failed to add flow with checkout.'));
+        dispatch(setStripeCheckoutError('Failed to add flow with checkout.'));
+      }
+      dispatch(setStripeCheckoutLoading(false));
+      dispatch(setFlowAddLoading(false));
+      return false;
+    }
+  }
+);
