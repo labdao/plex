ccab0d2643a9728f41b6d92f37468368c3eb4394
supraja-968 <supraja968@gmail.com>
2024-06-24 17:20:00 +0200

    db script update

--- gateway/migrations/32_tool_flow_to_model_exp_rename.up.sql
+++ gateway/migrations/32_tool_flow_to_model_exp_rename.up.sql
@@ -1,21 +1,22 @@
 -- RENAME TABLE TOOL TO MODEL
-ALTER TABLE tool RENAME TO model;
+ALTER TABLE tools RENAME TO models;
 
-ALTER TABLE model RENAME COLUMN default_tool TO default_model;
-ALTER TABLE model RENAME COLUMN tool_json TO model_json;
-ALTER TABLE model RENAME COLUMN tool_type TO model_type;
+ALTER TABLE models RENAME COLUMN default_tool TO default_model;
+ALTER TABLE models RENAME COLUMN tool_json TO model_json;
+ALTER TABLE models RENAME COLUMN tool_type TO model_type;
 
 --rename tool referenced id column in jobs table. also the foreign key constraint
 ALTER TABLE jobs RENAME COLUMN tool_id TO model_id;
-ALTER TABLE jobs DROP CONSTRAINT jobs_tool_id_fkey;
-ALTER TABLE jobs ADD CONSTRAINT jobs_model_id_fkey FOREIGN KEY (model_id) REFERENCES model(id) ON DELETE CASCADE;
+ALTER TABLE jobs DROP CONSTRAINT fk_jobs_tool;
+ALTER TABLE jobs ADD CONSTRAINT fk_jobs_model FOREIGN KEY (model_id) REFERENCES models(cid) ON DELETE CASCADE;
 
 --same with flow to experiment
-ALTER TABLE flow RENAME TO experiment;
+ALTER TABLE flows RENAME TO experiments;
 
-ALTER TABLE experiment RENAME COLUMN flow_uuid TO experiment_uuid;
+ALTER TABLE experiments RENAME COLUMN flow_uuid TO experiment_uuid;
 
 --in jobs
 ALTER TABLE jobs RENAME COLUMN flow_id TO experiment_id;
-ALTER TABLE jobs DROP CONSTRAINT jobs_flow_id_fkey;
-ALTER TABLE jobs ADD CONSTRAINT jobs_experiment_id_fkey FOREIGN KEY (experiment_id) REFERENCES experiment(id) ON DELETE CASCADE;
+ALTER TABLE jobs DROP CONSTRAINT IF EXISTS jobs_flowid_fkey;
+ALTER TABLE jobs DROP CONSTRAINT IF EXISTS fk_flows_jobs;
+ALTER TABLE jobs ADD CONSTRAINT jobs_experimentid_fkey FOREIGN KEY (experiment_id) REFERENCES experiments(id) ON DELETE CASCADE;
