9dda048029941d0644617538d0e9e9a301ad24d4
supraja-968 <supraja968@gmail.com>
2024-06-25 11:53:44 +0200

    colabdesign manifest fix, added * to required fields, added metrics description to manifest - frontend logic to follow later

--- gateway/app.go
+++ gateway/app.go
@@ -30,9 +30,9 @@ func ServeWebApp() {
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
-			SlowThreshold: time.Second,  // Slow SQL threshold
-			LogLevel:      logger.Error, // Log level
-			Colorful:      true,         // Enable color
+			SlowThreshold: time.Second, // Slow SQL threshold
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // Enable color
 		},
 	)
 
