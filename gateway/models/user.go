300c1acc006ea3b6fd4fc30aba4a43b56507e54f
Aakaash Meduri <aakaash.meduri@gmail.com>
2024-07-15 02:33:22 -0400

    unique subscription id

--- gateway/models/user.go
+++ gateway/models/user.go
@@ -18,9 +18,9 @@ type User struct {
 	UserFiles          []File       `gorm:"many2many:user_files;foreignKey:WalletAddress;joinForeignKey:wallet_address;inverseJoinForeignKey:file_id"`
 	Tier               Tier         `gorm:"type:int;not null;default:0" json:"tier"`
 	ComputeTally       int          `gorm:"column:compute_tally;default:0" json:"computeTally"`
-	StripeUserID       *string      `gorm:"column:stripe_user_id;type:varchar(255)" json:"stripeUserId"`
+	StripeUserID       string       `gorm:"column:stripe_user_id;type:varchar(255)" json:"stripeUserId"`
 	OrganizationID     uint         `gorm:"column:organization_id"`
 	Organization       Organization `gorm:"foreignKey:OrganizationID"`
-	SubscriptionStatus *string      `gorm:"column:subscription_status;type:varchar(255)" json:"subscriptionStatus"`
-	SubscriptionID     *string      `gorm:"column:subscription_id;type:varchar(255)" json:"subscriptionId"`
+	SubscriptionStatus string       `gorm:"column:subscription_status;type:varchar(255)" json:"subscriptionStatus"`
+	SubscriptionID     string       `gorm:"column:subscription_id;type:varchar(255);unique" json:"subscriptionId"`
 }
