4ee70264bc2927b4072393f529673046d9ee0bd1
alabdao <139594838+alabdao@users.noreply.github.com>
2024-07-17 17:10:52 -0400

    removed old terraform code (#998)

--- infrastructure/terraform/staging/acm.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_acm_certificate" "wildcard_labdao" {
-  domain_name               = "bacalhau.${var.environment}.${var.domain}"
-  subject_alternative_names = ["*.${var.environment}.${var.domain}"]
-  validation_method         = "DNS"
-}
