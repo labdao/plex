05fb03a7e0882243fff0de36c57a82ec425dc467
supraja-968 <supraja968@gmail.com>
2024-07-29 12:14:38 +0200

    updated page.tsx for subscribe page

--- frontend/app/subscribe/page.tsx
+++ frontend/app/subscribe/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Button } from "@/components/ui/button";
 import { Breadcrumbs } from "@/components/global/Breadcrumbs";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function SubscribePage() {
   const [walletAddress, setWalletAddress] = useState<string | null>(null);
@@ -30,46 +31,42 @@ export default function SubscribePage() {
         ]}
         actions={null}
       />
-      <div className="flex flex-col items-center justify-between w-[706px] h-[469px] p-4 md:p-6 lg:p-8 bg-white rounded-lg shadow-lg mx-auto my-6">
-        <h1
-          className="text-center"
-          style={{ fontFamily: '"Space Grotesk"', fontSize: '48px', fontWeight: 500, lineHeight: '43.2px', color: '#4C4C4C' }}
-        >
-          Become a lab.bio Subscriber
-        </h1>
-        <ul className="space-y-2 w-full">
-          <li className="flex items-center" style={{ color: '#4C4C4C', fontFamily: '"Space Grotesk"', fontSize: '20px', fontWeight: 500, lineHeight: '21.1px', letterSpacing: '0.1px' }}>
-            <span className="mr-2 text-green-500">✓</span>
+      <div className="flex flex-col items-center justify-between w-[706px] h-[469px] p-4 bg-white rounded-lg shadow-lg mx-auto my-6">
+        <h3 className="text-center font-heading" style={{ fontSize: '29px', lineHeight: '43.2px', letterSpacing: '0.5px', color: '#000000' }}>
+          Become a lab.bio subscriber
+        </h3>
+        <ul className="space-y-4 w-full font-heading" style={{ fontSize: '16px', lineHeight: '28px', letterSpacing: '0.3px', color: '#000000' }}>
+          <li className="flex items-center">
+            <span className="mr-2 text-black">✓</span>
             <span>Access x# of computation credits (about x number per x number)</span>
           </li>
-          <li className="flex items-center" style={{ color: '#4C4C4C', fontFamily: '"Space Grotesk"', fontSize: '20px', fontWeight: 500, lineHeight: '21.1px', letterSpacing: '0.1px' }}>
-            <span className="mr-2 text-green-500">✓</span>
+          <li className="flex items-center">
+            <span className="mr-2 text-black">✓</span>
             <span>Additional charges information Additional charges information</span>
           </li>
-          <li className="flex items-center" style={{ color: '#4C4C4C', fontFamily: '"Space Grotesk"', fontSize: '20px', fontWeight: 500, lineHeight: '21.1px', letterSpacing: '0.1px' }}>
-            <span className="mr-2 text-green-500">✓</span>
+          <li className="flex items-center">
+            <span className="mr-2 text-black">✓</span>
             <span>Additional charges information Additional charges information</span>
           </li>
-          <li className="flex items-center" style={{ color: '#4C4C4C', fontFamily: '"Space Grotesk"', fontSize: '20px', fontWeight: 500, lineHeight: '21.1px', letterSpacing: '0.1px' }}>
-            <span className="mr-2 text-green-500">✓</span>
+          <li className="flex items-center">
+            <span className="mr-2 text-black">✓</span>
             <span>Cancel subscription any time</span>
           </li>
         </ul>
-        <div className="text-center">
-          <p
-            className="mt-4"
-            style={{ fontFamily: '"Space Grotesk"', fontSize: '29px', fontWeight: 500, lineHeight: '30.595px', letterSpacing: '0.145px', color: '#4C4C4C' }}
-          >
-            X$ per month
-          </p>
-          <button
-            className="flex items-center justify-center mt-4 w-[489px] h-[42px] px-[26.853px] py-[20.139px] rounded-[53.705px] bg-gradient-to-r from-[rgba(107,219,173,0.5)] to-[rgba(7,164,198,0.5)] shadow-lg"
-            style={{ boxShadow: '67.131px 83.914px 167.828px 33.566px rgba(0, 0, 0, 0.25)' }}
-          >
-            Start Now
-          </button>
+        <p className="mt-4 text-center font-heading" style={{ fontSize: '24px', lineHeight: '30px', letterSpacing: '0.14px', color: '#000000', fontWeight: '500' }}>
+          X$ per month
+        </p>
+        <div className="px-2 py-2 w-full">
+          <Button asChild color="primary" size="sm" className="w-full font-bold">
+            <Link href="/subscribe/payment">
+              Start now
+            </Link>
+          </Button>
         </div>
+
+
       </div>
     </div>
   );
+  
 }
